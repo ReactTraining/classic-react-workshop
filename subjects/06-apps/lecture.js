21253e0ada8630b60cfa41133ea6c8713ff99336
Michael Jackson <mjijackson@gmail.com>
2015-04-09 15:40:05 -0700

    Add lecture stubs

--- subjects/06-apps/lecture.js
+++ subjects/06-apps/lecture.js
@@ -1,70 +1,12 @@
 var React = require('react');
-
-var PropTypes = {
-  product: React.PropTypes.shape({
-    name: React.PropTypes.string.isRequired,
-    price: React.PropTypes.number.isRequired,
-    inStock: React.PropTypes.bool
-  })
-};
-
-var ProductTable = React.createClass({
-  propTypes: {
-    category: React.PropTypes.string,
-    products: React.PropTypes.arrayOf(PropTypes.product)
-  },
-  render() {
-    return (
-      <div>
-        <h2>{this.props.category}</h2>
-        <table>
-          <thead>
-            
-          </thead>
-          <tbody>
-            {this.props.products.map((p, i) => <ProductRow key={i} product={p}/>)}
-          </tbody>
-        </table>
-      </div>
-    );
-  }
-});
-
-var CategoryRow = React.createClass({
-  propTypes: {
-    category: React.PropTypes.string.isRequired
-  },
-  render() {
-    return (
-      <tr>
-        <th colspan="2">{this.props.category}</th>
-      </tr>
-    );
-  }
-});
-
-var ProductRow = React.createClass({
-  propTypes: {
-    product: PropTypes.product
-  },
-  render() {
-    var { name, price } = this.props.product;
-
-    return (
-      <tr>
-        <td>{name}</td>
-        <td>${price/100}</td>
-      </tr>
-    );
-  }
-});
+var escapeRegExp = require('./utils/escapeRegExp');
 
 var CATALOG = [
   {
     category: 'Sporting Goods',
     products: [
       { name: 'Basketball', price: 4000 },
-      { name: 'Boxing Gloves', price: 3500 },
+      { name: 'Boxing Gloves', price: 3500, inStock: true },
       { name: 'Baseball', price: 1000 }
     ]
   },
@@ -72,40 +14,8 @@ var CATALOG = [
     category: 'Pets',
     products: [
       { name: 'Gerbil', price: 500 },
-      { name: 'Goldfish', price: 300 },
-      { name: 'Parakeet', price: 2000 }
+      { name: 'Goldfish', price: 300, inStock: true },
+      { name: 'Parakeet', price: 2000, inStock: true }
     ]
   }
 ];
-
-var App = React.createClass({
-  getInitialState() {
-    return {
-      searchQuery: '',
-      onlyShowInStock: false
-    };
-  },
-  handleQueryChange(event) {
-    this.setState({
-      searchQuery: event.target.value
-    });
-  },
-  handleInStockFilterChange(event) {
-    this.setState({
-      onlyShowInStock: event.target.checked
-    });
-  },
-  render() {
-    return (
-      <div>
-        <input ref="search" type="text" onChange={this.handleQueryChange} value={this.state.searchQuery}/>
-        <input ref="inStock" type="checkbox" onChange={this.handleInStockFilterChange} value={this.state.onlyShowInStock}/> Only show in stock
-        <div>
-          {CATALOG.map((props, i) => <ProductTable key={i} {...props}/>)}
-        </div>
-      </div>
-    );
-  }
-});
-
-React.render(<App/>, document.getElementById('app'));
