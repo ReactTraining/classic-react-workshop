445edfc63084da6d8e9497a3052550a8429488a2
Ryan Florence <rpflorence@gmail.com>
2015-04-08 23:18:15 -0700

    03 exercise

--- subjects/03-components/solution.js
+++ subjects/03-components/solution.js
@@ -35,17 +35,39 @@ styles.panel = {
 };
 
 var Tabs = React.createClass({
+  getInitialState () {
+    return {
+      activeIndex: 0
+    };
+  },
+
+  handleTabClick (index) {
+    this.setState({
+      activeIndex: index
+    });
+  },
+
+  renderTabs () {
+    return this.props.data.map((item, index) => {
+      var style = index === this.state.activeIndex ?
+        styles.activeTab : styles.tab;
+      return <div
+        className="Tab"
+        style={style}
+        onClick={this.handleTabClick.bind(this, index)}
+        children={item.name}
+      />;
+    });
+  },
+
   render () {
     return (
       <div className="Tabs">
-        <div className="Tab" style={styles.tab}>
-          Inactive
-        </div>
-        <div className="Tab" style={styles.activeTab}>
-          Active
+        <div>
+          {this.renderTabs()}
         </div>
         <div className="TabPanels" style={styles.panel}>
-          Panel
+          {this.props.data[this.state.activeIndex].description}
         </div>
       </div>
     );
---
+++ subjects/03-components/tests.js
@@ -0,0 +1,35 @@
+var assert = require('../shared/assert');
+var React = require('react/addons');
+var { click } = React.addons.TestUtils.Simulate;
+
+exports.run = () => {
+  var html = document.getElementById('app').innerHTML;
+  var tabs = document.querySelectorAll('.Tab');
+  var borderFixture = document.createElement('div');
+  borderFixture.setAttribute('style', 'border-bottom-color: #000;');
+
+  console.log('on first render');
+  assert(!!html.match(/USA/), 'render USA tab');
+  assert(!!html.match(/China/), 'render China tab');
+  assert(!!html.match(/Russia/), 'render Russia tab');
+  assert(
+    tabs[0].style.borderBottomColor === borderFixture.style.borderBottomColor,
+    'first tab is active'
+  );
+  assert(
+    tabs[1].style.borderBottomColor !== borderFixture.style.borderBottomColor,
+    'second tab is inactive'
+  );
+
+  console.log('after clicking the third tab...');
+  click(tabs[3]);
+  assert(
+    tabs[3].style.borderBottomColor === borderFixture.style.borderBottomColor,
+    'third tab is active'
+  );
+  assert(
+    tabs[0].style.borderBottomColor !== borderFixture.style.borderBottomColor,
+    'first tab is inactive'
+  );
+};
+
