15bc67a9bab6aa3c093de5f4f9eaa390fa091d23
Michael Jackson <mjijackson@gmail.com>
2015-04-08 18:21:06 -0700

    Finish lecture notes on encapsulating UI

--- subjects/03-components/lecture.js
+++ subjects/03-components/lecture.js
@@ -1,136 +1,381 @@
 import React from 'react';
 
+////////////////////////////////////////////////////////////////////////////////
+// Let's encapsulate state in an object and call it what it really is. Then, add
+// a setState function that we can use to update state and automatically update
+// the page any time the state changes.
+
+// var state = {
+//   isOpen: false
+// };
+//
+// function setState(nextState) {
+//   for (var property in nextState)
+//     if (nextState.hasOwnProperty(property))
+//       state[property] = nextState[property];
+//
+//   updateThePage();
+// }
+//
+// function handleClick() {
+//   setState({
+//     isOpen: !state.isOpen
+//   });
+// }
+//
+// function render() {
+//   return (
+//     <div className="ContentToggle">
+//       <button onClick={handleClick} className="ContentToggle__Summary">
+//         Tacos
+//       </button>
+//       <div className="ContentToggle__Details">
+//         {state.isOpen && (
+//           <p>
+//             A taco is a traditional Mexican dish composed of a corn or wheat
+//             tortilla folded or rolled around a filling. A taco can be made with a
+//             variety of fillings, including beef, pork, chicken, seafood,
+//             vegetables and cheese, allowing for great versatility and variety. A
+//             taco is generally eaten without utensils and is often accompanied
+//             by garnishes such as salsa, avocado or guacamole, cilantro
+//             (coriander), tomatoes, minced meat, onions and lettuce.
+//           </p>
+//         )}
+//       </div>
+//     </div>
+//   );
+// }
+//
+// function updateThePage() {
+//   React.render(render(), document.getElementById('app'));
+// }
+//
+// updateThePage();
 
 ////////////////////////////////////////////////////////////////////////////////
-// Instead of manipulating the DOM, we can modify our render() function so that
-// it just returns a description of what the page should look like. Then we can
-// call it as often as we want, whenever things change.
-var isOpen = false;
+// React gives us setState and automatically re-renders as the state changes
 
-function handleClick() {
-  isOpen = !isOpen;
-  updateThePage();
-}
-
-function render() {
-  return (
-    <div className="ContentToggle">
-      <div onClick={handleClick} className="ContentToggle__Summary">
-        Tacos
-      </div>
-      <div className="ContentToggle__Details">
-        {isOpen && (
-          <p>
-            A taco is a traditional Mexican dish composed of a corn or wheat
-            tortilla folded or rolled around a filling. A taco can be made with a
-            variety of fillings, including beef, pork, chicken, seafood,
-            vegetables and cheese, allowing for great versatility and variety. A
-            taco is generally eaten without utensils and is often accompanied
-            by garnishes such as salsa, avocado or guacamole, cilantro
-            (coriander), tomatoes, minced meat, onions and lettuce.
-          </p>
-        )}
-      </div>
-    </div>
-  );
-}
-
-function updateThePage() {
-  React.render(render(), document.getElementById('app'));
-}
-
-updateThePage();
+// var ContentToggle = React.createClass({
+//
+//   getInitialState() {
+//     return {
+//       isOpen: false
+//     };
+//   },
+//
+//   handleClick() {
+//     this.setState({
+//       isOpen: !this.state.isOpen
+//     });
+//   },
+//
+//   render() {
+//     return (
+//       <div className="ContentToggle">
+//         <button onClick={this.handleClick} className="ContentToggle__Summary">
+//           Tacos
+//         </button>
+//         <div className="ContentToggle__Details">
+//           {this.state.isOpen && (
+//             <p>
+//               A taco is a traditional Mexican dish composed of a corn or wheat
+//               tortilla folded or rolled around a filling. A taco can be made with a
+//               variety of fillings, including beef, pork, chicken, seafood,
+//               vegetables and cheese, allowing for great versatility and variety. A
+//               taco is generally eaten without utensils and is often accompanied
+//               by garnishes such as salsa, avocado or guacamole, cilantro
+//               (coriander), tomatoes, minced meat, onions and lettuce.
+//             </p>
+//           )}
+//         </div>
+//       </div>
+//     );
+//   }
+//
+// });
+//
+// React.render(<ContentToggle/>, document.getElementById('app'));
 
 ////////////////////////////////////////////////////////////////////////////////
-// Let's add a setState function that we can use to update state and
-// automatically update the page
+// Wrap <ContentToggle> in an <App> that tracks the # of times it has been
+// toggled and shows a counter. ContentToggle gets an onToggle handler, declared
+// as a function in propTypes
+// Real use case might be analytics
 
-var state = {
-  isOpen: false
+// var ContentToggle = React.createClass({
+//
+//   propTypes: {
+//     onToggle: React.PropTypes.func
+//   },
+//
+//   getInitialState() {
+//     return {
+//       isOpen: false
+//     };
+//   },
+//
+//   handleClick() {
+//     this.setState({
+//       isOpen: !this.state.isOpen
+//     });
+//
+//     if (this.props.onToggle)
+//       this.props.onToggle();
+//   },
+//
+//   render() {
+//     return (
+//       <div className="ContentToggle">
+//         <button onClick={this.handleClick} className="ContentToggle__Summary">
+//           Tacos
+//         </button>
+//         <div className="ContentToggle__Details">
+//           {this.state.isOpen && (
+//             <p>
+//               A taco is a traditional Mexican dish composed of a corn or wheat
+//               tortilla folded or rolled around a filling. A taco can be made with a
+//               variety of fillings, including beef, pork, chicken, seafood,
+//               vegetables and cheese, allowing for great versatility and variety. A
+//               taco is generally eaten without utensils and is often accompanied
+//               by garnishes such as salsa, avocado or guacamole, cilantro
+//               (coriander), tomatoes, minced meat, onions and lettuce.
+//             </p>
+//           )}
+//         </div>
+//       </div>
+//     );
+//   }
+//
+// });
+//
+// var App = React.createClass({
+//
+//   getInitialState() {
+//     return {
+//       numToggles: 0
+//     };
+//   },
+//
+//   handleToggle() {
+//     this.setState({
+//       numToggles: this.state.numToggles + 1
+//     });
+//   },
+//
+//   render() {
+//     return (
+//       <div>
+//         <p>Toggle count: {this.state.numToggles}</p>
+//         <ContentToggle onToggle={this.handleToggle}/>
+//       </div>
+//     );
+//   }
+//
+// });
+//
+// React.render(<App/>, document.getElementById('app'));
+
+////////////////////////////////////////////////////////////////////////////////
+// React expresses your application UI as a function of state. Props are like
+// arguments to the function. They look like element attributes in JSX
+
+// import MD5 from 'MD5';
+//
+// var GRAVATAR_URL = 'http://gravatar.com/avatar';
+// var USERS = [
+//   { name: 'Ryan Florence', email: 'rpflorence@gmail.com' },
+//   { name: 'Michael Jackson', email: 'mjijackson@gmail.com' }
+// ];
+//
+// var App = React.createClass({
+//   render () {
+//     return (
+//       <div>
+//         <h1>Users</h1>
+//         <ul>
+//         {USERS.map(user => (
+//           <li key={user.email}>
+//             <img src={GRAVATAR_URL + '/' + MD5(user.email)}/> {user.name}
+//           </li>
+//         ))}
+//         </ul>
+//       </div>
+//     );
+//   }
+// });
+//
+// React.render(<App/>, document.getElementById('app'));
+
+////////////////////////////////////////////////////////////////////////////////
+// We can refactor to get a <Gravatar> component that takes a user's email as
+// a prop and automatically generates the <img src> and a size prop
+
+// import MD5 from 'MD5';
+//
+// var GRAVATAR_URL = 'http://gravatar.com/avatar';
+// var USERS = [
+//   { name: 'Ryan Florence', email: 'rpflorence@gmail.com' },
+//   { name: 'Michael Jackson', email: 'mjijackson@gmail.com' }
+// ];
+//
+// var Gravatar = React.createClass({
+//   propTypes: {
+//     email: React.PropTypes.string.isRequired,
+//     size: React.PropTypes.number.isRequired
+//   },
+//   getDefaultProps() {
+//     return {
+//       size: 80 // this.props.size defaults to 80
+//     };
+//   },
+//   render() {
+//     return (
+//       <img src={GRAVATAR_URL + '/' + MD5(this.props.email) + '?s=' + this.props.size}/>
+//     );
+//   }
+// });
+//
+// var App = React.createClass({
+//   render () {
+//     return (
+//       <div>
+//         <h1>Users</h1>
+//         <ul>
+//         {USERS.map(user => (
+//           <li key={user.email}>
+//             <Gravatar email={user.email}/> {user.name}
+//           </li>
+//         ))}
+//         </ul>
+//       </div>
+//     );
+//   }
+// });
+//
+// React.render(<App/>, document.getElementById('app'));
+
+////////////////////////////////////////////////////////////////////////////////
+// We can have better validation with a custom prop
+
+// import MD5 from 'MD5';
+//
+// var GRAVATAR_URL = 'http://gravatar.com/avatar';
+// var USERS = [
+//   { name: 'Ryan Florence', email: 'rpflorence@gmail.com' },
+//   { name: 'Michael Jackson', email: 'mjijackson@gmail.com' }
+// ];
+//
+// function isEmail(value) {
+//   return typeof value === 'string' && (/[\w.-]+@[\w.-]+\.[a-zA-Z]{2,4}/).test(value);
+// }
+//
+// var Gravatar = React.createClass({
+//   propTypes: {
+//     email: function (props, propName, componentName) {
+//       var value = props[propName];
+//
+//       if (value !== undefined && !isEmail(value))
+//         return new Error(`Invalid prop "${propName}" given to "${componentName}", "${value}" does not look like an email address"`);
+//     },
+//     size: React.PropTypes.number.isRequired
+//   },
+//   getDefaultProps() {
+//     return {
+//       size: 80
+//     };
+//   },
+//   render() {
+//     return (
+//       <img src={GRAVATAR_URL + '/' + MD5(this.props.email) + '?s=' + this.props.size}/>
+//     );
+//   }
+// });
+//
+// var App = React.createClass({
+//   render () {
+//     return (
+//       <div>
+//         <h1>Users</h1>
+//         <ul>
+//         {USERS.map(user => (
+//           <li key={user.email}>
+//             <Gravatar email={user.email}/> {user.name}
+//           </li>
+//         ))}
+//         </ul>
+//       </div>
+//     );
+//   }
+// });
+//
+// React.render(<App/>, document.getElementById('app'));
+
+////////////////////////////////////////////////////////////////////////////////
+// Eventually you'll probably end up creating a bunch of custom prop types for
+// common types of objects you pass around in your app
+
+function isEmail(value) {
+  return typeof value === 'string' && (/[\w.-]+@[\w.-]+\.[a-zA-Z]{2,4}/).test(value);
+}
+
+var PropTypes = {
+
+  email: function (props, propName, componentName, location, isRequired) {
+    var value = props[propName];
+
+    if (isRequired && value === undefined)
+      return new Error(`Required prop "${propName}" was not specified in "${componentName}".`);
+
+    if (value !== undefined && !isEmail(value))
+      return new Error(`Invalid prop "${propName}" given to "${componentName}", "${value}" does not look like an email address"`);
+  }
+
 };
 
-function setState(nextState) {
-  for (var property in nextState)
-    if (nextState.hasOwnProperty(property))
-      state[property] = nextState[property];
+PropTypes.email.isRequired = function (...args) {
+  return PropTypes.email.apply(PropTypes.email, args.concat([ true ]));
+};
 
-  updateThePage();
-}
+import MD5 from 'MD5';
 
-function handleClick() {
-  setState({
-    isOpen: !state.isOpen
-  });
-}
+var GRAVATAR_URL = 'http://gravatar.com/avatar';
+var USERS = [
+  { name: 'Ryan Florence', email: 'rpflorence@gmail.com' },
+  { name: 'Michael Jackson', email: 'mjijackson@gmail.com' }
+];
 
-function render() {
-  return (
-    <div className="ContentToggle">
-      <div onClick={handleClick} className="ContentToggle__Summary">
-        Tacos
-      </div>
-      <div className="ContentToggle__Details">
-        {state.isOpen && (
-          <p>
-            A taco is a traditional Mexican dish composed of a corn or wheat
-            tortilla folded or rolled around a filling. A taco can be made with a
-            variety of fillings, including beef, pork, chicken, seafood,
-            vegetables and cheese, allowing for great versatility and variety. A
-            taco is generally eaten without utensils and is often accompanied
-            by garnishes such as salsa, avocado or guacamole, cilantro
-            (coriander), tomatoes, minced meat, onions and lettuce.
-          </p>
-        )}
-      </div>
-    </div>
-  );
-}
-
-function updateThePage() {
-  React.render(render(), document.getElementById('app'));
-}
-
-updateThePage();
-
-////////////////////////////////////////////////////////////////////////////////
-
-var ContentToggle = React.createClass({
-
-  getInitialState: function () {
+var Gravatar = React.createClass({
+  propTypes: {
+    email: PropTypes.email.isRequired,
+    size: React.PropTypes.number.isRequired
+  },
+  getDefaultProps() {
     return {
-      isOpen: false
+      size: 80
     };
   },
-
-  handleClick: function (event) {
-    event.preventDefault();
-
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
-  },
-
-  render: function () {
+  render() {
     return (
-      <div className="ContentToggle">
-        <div onClick={this.handleClick} className="ContentToggle__Summary">
-          Tacos
-        </div>
-        <div className="ContentToggle__Details">
-          {isOpen && (
-            <p>
-              A taco is a traditional Mexican dish composed of a corn or wheat
-              tortilla folded or rolled around a filling. A taco can be made with a
-              variety of fillings, including beef, pork, chicken, seafood,
-              vegetables and cheese, allowing for great versatility and variety. A
-              taco is generally eaten without utensils and is often accompanied
-              by garnishes such as salsa, avocado or guacamole, cilantro
-              (coriander), tomatoes, minced meat, onions and lettuce.
-            </p>
-          )}
-        </div>
+      <img src={GRAVATAR_URL + '/' + MD5(this.props.email) + '?s=' + this.props.size}/>
+    );
+  }
+});
+
+var App = React.createClass({
+  render () {
+    return (
+      <div>
+        <h1>Users</h1>
+        <ul>
+        {USERS.map(user => (
+          <li key={user.email}>
+            <Gravatar email={user.email}/> {user.name}
+          </li>
+        ))}
+        </ul>
       </div>
     );
   }
-
 });
+
+React.render(<App/>, document.getElementById('app'));
