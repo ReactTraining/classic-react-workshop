88bd1f19ee95ef3437dd30b314515a3d68ce9ae7
Michael Jackson <mjijackson@gmail.com>
2015-07-24 14:37:38 -0700

    Style tweaks

--- subjects/13-virtual-dom/lecture.js
+++ subjects/13-virtual-dom/lecture.js
@@ -8,33 +8,43 @@ var style = {
 };
 
 var Factoid = React.createClass({
-  getInitialState () {
-    return { text: '' };
+
+  getInitialState() {
+    return {
+      text: ''
+    };
   },
 
-  handleInputChange (event) {
-    this.setState({ text: event.target.value });
+  handleInputChange(event) {
+    this.setState({
+      text: event.target.value
+    });
   },
 
-  render () {
-    var tokens = this.state.text.split(' ').map((word, index, arr) => {
-      if (word.trim() === '') return null;
+  render() {
+    var { text } = this.state;
+
+    var tokens = text.split(' ').map((word, index, arr) => {
+      if (word.trim() === '')
+        return null;
+
       return [
         <div style={style}>{word}</div>,
         arr.length - 1 === index ? null : '+'
       ];
     });
+
     return (
       <div>
-        <input onChange={this.handleInputChange}/>
-        <div style={{marginTop: 20}}/>
+        <input onChange={this.handleInputChange} />
+        <div style={{ marginTop: 20 }} />
         <div>
           {tokens}
         </div>
       </div>
     );
   }
+
 });
 
-React.render(<Factoid/>, document.getElementById('app'));
-
+React.render(<Factoid />, document.getElementById('app'));
