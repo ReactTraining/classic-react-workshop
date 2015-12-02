bf09f0c7ebf63d5243764fe3b0a65e5997f9ba20
Michael Jackson <mjijackson@gmail.com>
2015-12-02 23:32:56 +0000

    Remove ContentToggle from rendering lecture

--- subjects/02-rendering/lecture.js
+++ subjects/02-rendering/lecture.js
@@ -151,49 +151,6 @@ import { render } from 'react-dom'
 //render(<MonthSelect />, document.getElementById('app'))
 
 
-//////////////////////////////////////////////////////////////////////////////////
-// Lets make a thing
-//let isOpen = false
-
-//function handleClick() {
-  //isOpen = !isOpen
-  //updateThePage()
-//}
-
-//function App() {
-  //let summaryClassName = 'ContentToggle__Summary'
-  //if (isOpen)
-    //summaryClassName += ' ContentToggle__Summary--is-open'
-
-  //return (
-    //<div className="ContentToggle">
-      //<button onClick={handleClick} className={summaryClassName}>
-        //Tacos
-      //</button>
-      //{isOpen && (
-        //<div className="ContentToggle__Details">
-          //<p>
-            //A taco is a traditional Mexican dish composed of a corn or wheat
-            //tortilla folded or rolled around a filling. A taco can be made with a
-            //variety of fillings, including beef, pork, chicken, seafood,
-            //vegetables and cheese, allowing for great versatility and variety. A
-            //taco is generally eaten without utensils and is often accompanied
-            //by garnishes such as salsa, avocado or guacamole, cilantro
-            //(coriander), tomatoes, minced meat, onions and lettuce.
-          //</p>
-        //</div>
-      //)}
-    //</div>
-  //)
-//}
-
-//function updateThePage() {
-  //render(<App />, document.getElementById('app'))
-//}
-
-//updateThePage()
-
-
 ////////////////////////////////////////////////////////////////////////////////
 // - Always rerender
 // - Virtual DOM makes it efficient
