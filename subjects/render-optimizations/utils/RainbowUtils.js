d7886cd77b2431e79695b51740aabd652a42c86e
Michael Jackson <mjijackson@gmail.com>
2015-12-03 20:28:04 +0000

    Style tweaks

--- subjects/render-optimizations/utils/RainbowUtils.js
+++ subjects/render-optimizations/utils/RainbowUtils.js
@@ -8,33 +8,33 @@ const teens = [ 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 's
 
 function convertMillions(n) {
   if (n >= 1000000)
-    return convertMillions(Math.floor(n / 1000000)) + " million " + convertThousands(n % 1000000)
+    return convertMillions(Math.floor(n / 1000000)) + ' million ' + convertThousands(n % 1000000)
 
   return convertThousands(n)
 }
 
 function convertThousands(n) {
   if (n >= 1000)
-    return convertHundreds(Math.floor(n / 1000)) + " thousand " + convertHundreds(n % 1000)
+    return convertHundreds(Math.floor(n / 1000)) + ' thousand ' + convertHundreds(n % 1000)
 
   return convertHundreds(n)
 }
 
 function convertHundreds(n) {
   if (n > 99)
-    return ones[Math.floor(n / 100)] + " hundred " + convertTens(n % 100)
+    return ones[Math.floor(n / 100)] + ' hundred ' + convertTens(n % 100)
 
   return convertTens(n)
 }
 
 function convertTens(n) {
-  if (n<10)
+  if (n < 10)
     return ones[n]
 
   if (n >= 10 && n < 20)
     return teens[n - 10]
 
-  return tens[Math.floor(n / 10)] + " " + ones[n % 10]
+  return tens[Math.floor(n / 10)] + ' ' + ones[n % 10]
 }
 
 export function convertNumberToEnglish(n) {
