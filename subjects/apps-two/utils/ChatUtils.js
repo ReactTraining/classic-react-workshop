2614b6bdc43c3f2f89a8fe8758d5b486a0150bac
Michael Jackson <mjijackson@gmail.com>
2015-12-03 08:52:33 +0000

    Syntax tweaks

--- subjects/apps-two/utils/ChatUtils.js
+++ subjects/apps-two/utils/ChatUtils.js
@@ -1,60 +1,60 @@
-const Firebase = require('firebase/lib/firebase-web');
+import invariant from 'invariant'
+import Firebase from 'firebase/lib/firebase-web'
 
-const ref = new Firebase('https://hip-react.firebaseio.com');
-
-let serverTimeOffset = 0;
+const ref = new Firebase('https://hip-react.firebaseio.com')
+let serverTimeOffset = 0
 
 ref.child('.info/serverTimeOffset').on('value', function (snapshot) {
-  serverTimeOffset = snapshot.val();
-});
+  serverTimeOffset = snapshot.val()
+})
 
 export function login(callback) {
-  ref.authWithOAuthPopup('github', callback);
+  const auth = ref.getAuth()
+  if (auth)
+    callback(null, auth)
+  else
+    ref.authWithOAuthPopup('github', callback)
 }
 
 export function sendMessage(channel, username, avatar, text) {
+  invariant(
+    channel && username && avatar && text,
+    'You must provide (channel, username, avatar, text) to sendMessage'
+  )
+
   ref.child(`channels/${channel}/messages`).push({
     timestamp: Date.now() + serverTimeOffset,
     username,
     avatar,
     text
-  });
-}
-
-export function deleteMessage(channel, key) {
-  console.log(channel, key);
-  ref.child(`channels/${channel}/messages/${key}`).set(null);
-}
-
-export function editMessage(channel, key, text) {
-  ref.child(`channels/${channel}/messages/${key}/text`).set(text);
+  })
 }
 
 function subscribeToList(path, callback) {
-  const child = ref.child(path).limitToLast(100);
-  const onChange = child.on('value', (snapshot) => {
-    const items = [];
+  function handleChange(snapshot) {
+    const items = []
 
-    snapshot.forEach(function (s, k) {
-      const item = s.val();
-      item._key = s.key();
-      items.push(item);
-    });
+    snapshot.forEach(function (s) {
+      const item = s.val()
+      item._key = s.key()
+      items.push(item)
+    })
 
-    callback(items);
-  });
+    callback(items)
+  }
 
-  return {
-    dispose() {
-      child.off('value', onChange);
-    }
-  };
+  const child = ref.child(path).limitToLast(100)
+  child.on('value', handleChange)
+
+  return function () {
+    child.off('value', handleChange)
+  }
 }
 
 export function subscribeToMessages(channel, callback) {
-  return subscribeToList(`channels/${channel}/messages`, callback);
+  return subscribeToList(`channels/${channel}/messages`, callback)
 }
 
 export function subscribeToChannels(callback) {
-  return subscribeToList(`channels`, callback);
+  return subscribeToList(`channels`, callback)
 }
--- subjects/apps/utils/ChatUtils.js
+++ subjects/apps/utils/ChatUtils.js
@@ -1,60 +1,60 @@
-const invariant = require('invariant');
-const Firebase = require('firebase/lib/firebase-web');
+import invariant from 'invariant'
+import Firebase from 'firebase/lib/firebase-web'
 
-const ref = new Firebase('https://hip-react.firebaseio.com');
-let serverTimeOffset = 0;
+const ref = new Firebase('https://hip-react.firebaseio.com')
+let serverTimeOffset = 0
 
 ref.child('.info/serverTimeOffset').on('value', function (snapshot) {
-  serverTimeOffset = snapshot.val();
-});
+  serverTimeOffset = snapshot.val()
+})
 
 export function login(callback) {
   const auth = ref.getAuth()
   if (auth)
     callback(null, auth)
   else
-    ref.authWithOAuthPopup('github', callback);
+    ref.authWithOAuthPopup('github', callback)
 }
 
 export function sendMessage(channel, username, avatar, text) {
   invariant(
     channel && username && avatar && text,
     'You must provide (channel, username, avatar, text) to sendMessage'
-  );
+  )
 
   ref.child(`channels/${channel}/messages`).push({
     timestamp: Date.now() + serverTimeOffset,
     username,
     avatar,
     text
-  });
+  })
 }
 
 function subscribeToList(path, callback) {
   function handleChange(snapshot) {
-    const items = [];
+    const items = []
 
     snapshot.forEach(function (s) {
-      const item = s.val();
-      item._key = s.key();
-      items.push(item);
-    });
+      const item = s.val()
+      item._key = s.key()
+      items.push(item)
+    })
 
-    callback(items);
+    callback(items)
   }
 
-  const child = ref.child(path).limitToLast(100);
-  child.on('value', handleChange);
+  const child = ref.child(path).limitToLast(100)
+  child.on('value', handleChange)
 
   return function () {
-    child.off('value', handleChange);
-  };
+    child.off('value', handleChange)
+  }
 }
 
 export function subscribeToMessages(channel, callback) {
-  return subscribeToList(`channels/${channel}/messages`, callback);
+  return subscribeToList(`channels/${channel}/messages`, callback)
 }
 
 export function subscribeToChannels(callback) {
-  return subscribeToList(`channels`, callback);
+  return subscribeToList(`channels`, callback)
 }
