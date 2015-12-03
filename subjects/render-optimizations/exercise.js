7dcc60a20d9c5b75ffdb80ae58e70a0a9b4d1279
Michael Jackson <mjijackson@gmail.com>
2015-12-03 14:48:55 +0000

    Update background

--- subjects/render-optimizations/exercise.js
+++ subjects/render-optimizations/exercise.js
@@ -38,6 +38,6 @@ const ListView = React.createClass({
 })
 
 render(
-  <RainbowList ListView={ListView} length={500} />,
+  <RainbowList ListView={ListView} length={500000} />,
   document.getElementById('app')
 )
