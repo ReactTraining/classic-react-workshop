f6ae10cfc8c90d0b01589c8886e5a482e2c57434
Michael Jackson <mjijackson@gmail.com>
2015-12-08 15:24:11 -0800

    Update components lecture

--- subjects/03-components/lecture.js
+++ subjects/03-components/lecture.js
@@ -17,11 +17,11 @@ function ContentToggle() {
   return (
     <div className="ContentToggle">
       <button onClick={handleClick} className={summaryClassName}>
-        Haggis
+        Tacos
       </button>
       {isOpen && (
         <div className="ContentToggle__Details">
-          <p>Haggis is a savoury pudding containing sheep's pluck (heart, liver and lungs); minced with onion, oatmeal, suet, spices, and salt, mixed with stock, traditionally encased in the animal's stomach though now often in an artificial casing instead. According to the 2001 English edition of the Larousse Gastronomique: "Although its description is not immediately appealing, haggis has an excellent nutty texture and delicious savoury flavour".</p>
+          <p>A taco is a traditional Mexican dish composed of a corn or wheat tortilla folded or rolled around a filling.</p>
         </div>
       )}
     </div>
@@ -45,6 +45,19 @@ updateThePage()
 ////////////////////////////////////////////////////////////////////////////////
 // Let's make <ContentToggle> re-usable and render a few of them. Title and
 // children are properties we can pass in from the parent component.
+
+////////////////////////////////////////////////////////////////////////////////
+// Wrap a few <ContentToggle>s in a <ToggleTracker> that tracks the # of times
+// it has been toggled and shows a counter. <ContentToggle> gets an onToggle
+// handler, declared as a function in propTypes.
+
+////////////////////////////////////////////////////////////////////////////////
+// But we just got finished making <ContentToggle> generic, and now
+// <ToggleTracker> is not! Can we make it generic as well? React.cloneElement
+// can help us pass props to elements that weren't initially provided.
+//
+// Side note: Be careful to use the React.Children utility methods.
+// this.props.children is opaque!
 
 //const ContentToggle = React.createClass({
 //
@@ -134,65 +147,3 @@ updateThePage()
 //    </ContentToggle>
 //  </ToggleTracker>
 //), document.getElementById('app'))
-
-////////////////////////////////////////////////////////////////////////////////
-// Wrap <ContentToggle> in a <ToggleTracker> that tracks the # of times it has
-// been toggled and shows a counter. <ContentToggle> gets an onToggle handler,
-// declared as a function in propTypes.
-
-////////////////////////////////////////////////////////////////////////////////
-// React expresses your application UI as a function of state. Props are like
-// arguments to the function. They look like element attributes in JSX.
-
-//import md5 from 'md5'
-//
-//const GravatarURL = 'http://gravatar.com/avatar'
-//
-//const Gravatar = React.createClass({
-//  propTypes: {
-//    email: React.PropTypes.string.isRequired
-//  },
-//  render() {
-//    const src = `${GravatarURL}/${md5(this.props.email)}?s=40`
-//    return <img src={src} />
-//  }
-//})
-//
-//const App = React.createClass({
-//  propTypes: {
-//    users: React.PropTypes.array
-//  },
-//  render() {
-//    return (
-//      <div>
-//        <h1>Users</h1>
-//        <ul>
-//          {this.props.users.map(user => (
-//            <li key={user.email}>
-//              <Gravatar email={user.email} /> {user.name}
-//            </li>
-//          ))}
-//        </ul>
-//      </div>
-//    )
-//  }
-//})
-//
-//const USERS = [
-//  { name: 'Ryan Florence', email: 'rpflorence@gmail.com' },
-//  { name: 'Michael Jackson', email: 'mjijackson@gmail.com' }
-//]
-//
-//render(<App users={USERS} />, document.getElementById('app'))
-
-////////////////////////////////////////////////////////////////////////////////
-// Let's use a <Gravatar> component that takes a user's email as a prop and
-// automatically generates the <img src> and a size prop. Add a default value
-// to the size prop to show it doesn't always need to be passed in.
-
-////////////////////////////////////////////////////////////////////////////////
-// Create a custom user propType for <App users>
-
-////////////////////////////////////////////////////////////////////////////////
-// Eventually you'll probably end up creating a bunch of custom prop types for
-// common types of objects you pass around in your app.
