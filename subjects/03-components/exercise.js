cc97f7268645f6cc78847a5cf9e3466b161321d9
Michael Jackson <mjijackson@gmail.com>
2015-12-08 20:00:00 -0800

    Add extra stuff to components exercise

--- subjects/03-components/exercise.js
+++ subjects/03-components/exercise.js
@@ -1,11 +1,16 @@
 ////////////////////////////////////////////////////////////////////////////////
 // Exercise:
 //
-// - Render the data as tabs, with their `name` as the label in the tab
-//   and their `description` inside the tab panel
-// - Make it so that you can click a tab label and the panel renders
-//   the correct content
-// - Make sure the active tab has the active styles
+// - Render a tab for each country with its name in the tab and
+//   its description in the panel
+// - Make it so that you can click on a tab and it will appear active
+//   while the others appear inactive
+// - Make it so the panel renders the correct content for the selected tab
+//
+// Got extra time?
+//
+// - Make <Tabs> generic so that it doesn't know anything about
+//   country data (Hint: good propTypes help)
 ////////////////////////////////////////////////////////////////////////////////
 import React from 'react'
 import { render } from 'react-dom'
