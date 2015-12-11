aba4eae3f31596aae3a320937fb9816b29092e6f
Michael Jackson <mjijackson@gmail.com>
2015-12-11 08:59:06 -0800

    Update lecture

--- subjects/06-compound-components/lecture.js
+++ subjects/06-compound-components/lecture.js
@@ -7,177 +7,167 @@ const { instanceOf, oneOf, array, arrayOf, shape, node, string } = React.PropTyp
 ////////////////////////////////////////////////////////////////////////////////
 // Let's make some tabs...
 
-//const Tabs = React.createClass({
+const Tabs = React.createClass({
+  getInitialState() {
+    return {
+      activeIndex: 0
+    }
+  },
+  selectTabIndex(activeIndex) {
+    this.setState({ activeIndex })
+  },
+  renderTabs() {
+    return this.props.data.map((tab, index) => {
+      const isActive = this.state.activeIndex === index
+      return (
+        <div
+          key={tab.label}
+          style={isActive ? styles.activeTab : styles.tab}
+          onClick={() => this.selectTabIndex(index)}
+        >{tab.label}</div>
+      )
+    })
+  },
+  renderPanel() {
+    const tab = this.props.data[this.state.activeIndex]
+    return (
+      <div>
+        <p>{tab.description}</p>
+      </div>
+    )
+  },
+  render() {
+    return (
+      <div>
+        <div style={styles.tabs}>
+          {this.renderTabs()}
+        </div>
+        <div style={styles.tabPanels}>
+          {this.renderPanel()}
+        </div>
+      </div>
+    )
+  }
+})
 
-  //getInitialState() {
-    //return {
-      //activeIndex: 0
-    //};
-  //},
+const App = React.createClass({
+  render() {
+    const tabData = [
+      {
+        label: 'Tacos',
+        description: <p>Tacos are delicious</p>
+      },
+      {
+        label: 'Burritos',
+        description: <p>Sometimes a burrito is what you really need.</p>
+      },
+      {
+        label: 'Coconut Korma',
+        description: <p>Might be your best option.</p>
+      }
+    ]
 
-  //handleTabClick(activeIndex) {
-    //this.setState({ activeIndex });
-  //},
+    return (
+      <div>
+        <Tabs data={tabData}/>
+      </div>
+    )
+  }
+})
 
-  //renderTabs() {
-    //return this.props.data.map((tab, index) => {
-      //const isActive = this.state.activeIndex === index;
-      //return (
-        //<div
-          //key={tab.label}
-          //style={isActive ? styles.activeTab : styles.tab}
-          //onClick={() => this.handleTabClick(index)}
-        //>{tab.label}</div>
-      //);
-    //});
-  //},
-
-  //renderPanel() {
-    //const tab = this.props.data[this.state.activeIndex];
-    //return (
-      //<div>
-        //<p>{tab.description}</p>
-      //</div>
-    //);
-  //},
-
-  //render() {
-    //return (
-      //<div>
-        //<div style={styles.tabs}>
-          //{this.renderTabs()}
-        //</div>
-        //<div style={styles.tabPanels}>
-          //{this.renderPanel()}
-        //</div>
-      //</div>
-    //);
-  //}
-
-//});
-
-//const App = React.createClass({
-
-  //render () {
-    //const tabData = [
-      //{
-        //label: 'Tacos',
-        //description: <p>Tacos are delicious</p>
-      //},
-      //{
-        //label: 'Burritos',
-        //description: <p>Sometimes a burrito is what you really need.</p>
-      //},
-      //{
-        //label: 'Coconut Korma',
-        //description: <p>Might be your best option.</p>
-      //},
-
-    //]
-
-    //return (
-      //<div>
-        //<Tabs data={tabData}/>
-      //</div>
-    //);
-  //}
-//});
-
-//render(<App/>, document.getElementById('app'))
-
+render(<App/>, document.getElementById('app'))
 
 ////////////////////////////////////////////////////////////////////////////////
 // What if I wanted tabs on the bottom?
 
 //const Tabs = React.createClass({
-
-  //getDefaultProps () {
-    //return {
-      //tabsPlacement: 'top'
-    //};
-  //},
-
-  //getInitialState() {
-    //return {
-      //activeIndex: 0
-    //};
-  //},
-
-  //handleTabClick(activeIndex) {
-    //this.setState({ activeIndex });
-  //},
-
-  //renderTabs() {
-    //return this.props.data.map((tab, index) => {
-      //const isActive = this.state.activeIndex === index;
-      //return (
-        //<div
-          //key={tab.label}
-          //style={isActive ? styles.activeTab : styles.tab}
-          //onClick={() => this.handleTabClick(index)}
-        //>{tab.label}</div>
-      //);
-    //});
-  //},
-
-  //renderPanel() {
-    //const tab = this.props.data[this.state.activeIndex];
-    //return (
-      //<div>
-        //<p>{tab.description}</p>
-      //</div>
-    //);
-  //},
-
-  //render() {
-    //const tabs = (
-      //<div key="tabs" style={styles.tabs}>
-        //{this.renderTabs()}
-      //</div>
-    //);
-    //const panel = (
-      //<div key="panel" style={styles.tabPanels}>
-        //{this.renderPanel()}
-      //</div>
-    //);
-    //return (
-      //<div>
-        //{this.props.tabsPlacement === 'top' ?
-          //[tabs, panel] :
-          //[panel, tabs]
-        //}
-      //</div>
-    //);
-  //}
-
-//});
-
+//
+//  getDefaultProps () {
+//    return {
+//      tabsPlacement: 'top'
+//    }
+//  },
+//
+//  getInitialState() {
+//    return {
+//      activeIndex: 0
+//    }
+//  },
+//
+//  selectTabIndex(activeIndex) {
+//    this.setState({ activeIndex })
+//  },
+//
+//  renderTabs() {
+//    return this.props.data.map((tab, index) => {
+//      const isActive = this.state.activeIndex === index
+//      return (
+//        <div
+//          key={tab.label}
+//          style={isActive ? styles.activeTab : styles.tab}
+//          onClick={() => this.selectTabIndex(index)}
+//        >{tab.label}</div>
+//      )
+//    })
+//  },
+//
+//  renderPanel() {
+//    const tab = this.props.data[this.state.activeIndex]
+//    return (
+//      <div>
+//        <p>{tab.description}</p>
+//      </div>
+//    )
+//  },
+//
+//  render() {
+//    const tabs = (
+//      <div key="tabs" style={styles.tabs}>
+//        {this.renderTabs()}
+//      </div>
+//    )
+//    const panel = (
+//      <div key="panel" style={styles.tabPanels}>
+//        {this.renderPanel()}
+//      </div>
+//    )
+//    return (
+//      <div>
+//        {this.props.tabsPlacement === 'top' ?
+//          [tabs, panel] :
+//          [panel, tabs]
+//        }
+//      </div>
+//    )
+//  }
+//
+//})
+//
 //const App = React.createClass({
-
-  //render () {
-    //const tabData = [
-      //{
-        //label: 'Tacos',
-        //description: <p>Tacos are delicious</p>
-      //},
-      //{
-        //label: 'Burritos',
-        //description: <p>Sometimes a burrito is what you really need.</p>
-      //},
-      //{
-        //label: 'Coconut Korma',
-        //description: <p>Might be your best option.</p>
-      //},
-    //]
-
-    //return (
-      //<div>
-        //<Tabs data={tabData} tabsPlacement="bottom" />
-      //</div>
-    //);
-  //}
-//});
-
+//  render() {
+//    const tabData = [
+//      {
+//        label: 'Tacos',
+//        description: <p>Tacos are delicious</p>
+//      },
+//      {
+//        label: 'Burritos',
+//        description: <p>Sometimes a burrito is what you really need.</p>
+//      },
+//      {
+//        label: 'Coconut Korma',
+//        description: <p>Might be your best option.</p>
+//      }
+//    ]
+//
+//    return (
+//      <div>
+//        <Tabs data={tabData} tabsPlacement="bottom" />
+//      </div>
+//    )
+//  }
+//})
+//
 //render(<App/>, document.getElementById('app'))
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -189,108 +179,106 @@ const { instanceOf, oneOf, array, arrayOf, shape, node, string } = React.PropTyp
 // - adding another option that has to do with rendering will add even more
 //   complexity
 
-
 ////////////////////////////////////////////////////////////////////////////////
 // Lets add "disabled" to a tab, what does jQuery UI do?
 // https://api.jqueryui.com/tabs/#option-disabled
 
 //const Tabs = React.createClass({
-
-  //getDefaultProps () {
-    //return {
-      //tabsPlacement: 'top',
-      //disabled: []
-    //};
-  //},
-
-  //getInitialState() {
-    //return {
-      //activeIndex: 0
-    //};
-  //},
-
-  //handleTabClick(activeIndex) {
-    //this.setState({ activeIndex });
-  //},
-
-  //renderTabs() {
-    //return this.props.data.map((tab, index) => {
-      //const isActive = this.state.activeIndex === index;
-      //const isDisabled = this.props.disabled.indexOf(index) !== -1
-      //const props = {
-        //key: tab.label,
-        //style: isDisabled ? styles.disabledTab : (
-          //isActive ? styles.activeTab : styles.tab
-        //)
-      //}
-      //if (!isDisabled)
-        //props.onClick = () => this.handleTabClick(index);
-      //return <div {...props}>{tab.label}</div>;
-    //});
-  //},
-
-  //renderPanel() {
-    //const tab = this.props.data[this.state.activeIndex];
-    //return (
-      //<div>
-        //<p>{tab.description}</p>
-      //</div>
-    //);
-  //},
-
-  //render() {
-    //const tabs = (
-      //<div key="tabs" style={styles.tabs}>
-        //{this.renderTabs()}
-      //</div>
-    //);
-    //const panel = (
-      //<div key="panel" style={styles.tabPanels}>
-        //{this.renderPanel()}
-      //</div>
-    //);
-    //return (
-      //<div>
-        //{this.props.tabsPlacement === 'top' ?
-          //[tabs, panel] :
-          //[panel, tabs]
-        //}
-      //</div>
-    //);
-  //}
-
-//});
-
+//
+//  getDefaultProps () {
+//    return {
+//      tabsPlacement: 'top',
+//      disabled: []
+//    }
+//  },
+//
+//  getInitialState() {
+//    return {
+//      activeIndex: 0
+//    }
+//  },
+//
+//  selectTabIndex(activeIndex) {
+//    this.setState({ activeIndex })
+//  },
+//
+//  renderTabs() {
+//    return this.props.data.map((tab, index) => {
+//      const isActive = this.state.activeIndex === index
+//      const isDisabled = this.props.disabled.indexOf(index) !== -1
+//      const props = {
+//        key: tab.label,
+//        style: isDisabled ? styles.disabledTab : (
+//          isActive ? styles.activeTab : styles.tab
+//        )
+//      }
+//      if (!isDisabled)
+//        props.onClick = () => this.selectTabIndex(index)
+//      return <div {...props}>{tab.label}</div>
+//    })
+//  },
+//
+//  renderPanel() {
+//    const tab = this.props.data[this.state.activeIndex]
+//    return (
+//      <div>
+//        <p>{tab.description}</p>
+//      </div>
+//    )
+//  },
+//
+//  render() {
+//    const tabs = (
+//      <div key="tabs" style={styles.tabs}>
+//        {this.renderTabs()}
+//      </div>
+//    )
+//    const panel = (
+//      <div key="panel" style={styles.tabPanels}>
+//        {this.renderPanel()}
+//      </div>
+//    )
+//    return (
+//      <div>
+//        {this.props.tabsPlacement === 'top' ?
+//          [tabs, panel] :
+//          [panel, tabs]
+//        }
+//      </div>
+//    )
+//  }
+//
+//})
+//
 //const App = React.createClass({
-
-  //render () {
-    //const tabData = [
-      //{
-        //label: 'Tacos',
-        //description: <p>Tacos are delicious</p>
-      //},
-      //{
-        //label: 'Burritos',
-        //description: <p>Sometimes a burrito is what you really need.</p>
-      //},
-      //{
-        //label: 'Coconut Korma',
-        //description: <p>Might be your best option.</p>
-      //},
-    //]
-
-    //return (
-      //<div>
-        //<Tabs
-          //data={tabData}
-          //tabsPlacement="top"
-          //disabled={[1]}
-        ///>
-      //</div>
-    //);
-  //}
-//});
-
+//  render() {
+//    const tabData = [
+//      {
+//        label: 'Tacos',
+//        description: <p>Tacos are delicious</p>
+//      },
+//      {
+//        label: 'Burritos',
+//        description: <p>Sometimes a burrito is what you really need.</p>
+//      },
+//      {
+//        label: 'Coconut Korma',
+//        description: <p>Might be your best option.</p>
+//      }
+//    ]
+//
+//    return (
+//      <div>
+//        <Tabs
+//          data={tabData}
+//          tabsPlacement="top"
+//          disabled={[1]}
+//        />
+//      </div>
+//    )
+//  }
+//})
+//
 //render(<App/>, document.getElementById('app'))
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -309,162 +297,163 @@ const { instanceOf, oneOf, array, arrayOf, shape, node, string } = React.PropTyp
 //    - in other words, we now have control over rendering while
 //      Tabs handles the interaction
 
-const TabList = React.createClass({
-  render() {
-    const children = React.Children.map(this.props.children, (child, index) => {
-      return React.cloneElement(child, {
-        isActive: index === this.props.activeIndex,
-        onClick: () => this.props.onActivate(index)
-      })
-    })
-
-    return <div style={styles.tabs}>{children}</div>
-  }
-})
-
-const Tab = React.createClass({
-  render() {
-    return (
-      <div
-        onClick={this.props.disabled ? null : this.props.onClick}
-        style={this.props.disabled ? styles.disabledTab : (
-          this.props.isActive ? styles.activeTab : styles.tab
-        )}
-      >
-        {this.props.children}
-      </div>
-    )
-  }
-})
-
-const TabPanels = React.createClass({
-  render() {
-    return (
-      <div style={styles.tabPanels}>
-        {this.props.children[this.props.activeIndex]}
-      </div>
-    )
-  }
-})
-
-const TabPanel = React.createClass({
-  render() {
-    return <div>{this.props.children}</div>
-  }
-})
-
-const Tabs = React.createClass({
-  getInitialState() {
-    return {
-      activeIndex: 0
-    }
-  },
-
-  render() {
-    const children = React.Children.map(this.props.children, (child, index) => {
-      if (child.type === TabPanels) {
-        return React.cloneElement(child, {
-          activeIndex: this.state.activeIndex
-        })
-      } else if (child.type === TabList) {
-        return React.cloneElement(child, {
-          activeIndex: this.state.activeIndex,
-          onActivate: (activeIndex) => this.setState({ activeIndex })
-        })
-      } else {
-        return child
-      }
-    })
-
-    return <div>{children}</div>
-  }
-})
-
+//const TabList = React.createClass({
+//  render() {
+//    const children = React.Children.map(this.props.children, (child, index) => {
+//      return React.cloneElement(child, {
+//        isActive: index === this.props.activeIndex,
+//        onClick: () => this.props.onActivate(index)
+//      })
+//    })
+//
+//    return <div style={styles.tabs}>{children}</div>
+//  }
+//})
+//
+//const Tab = React.createClass({
+//  render() {
+//    return (
+//      <div
+//        onClick={this.props.disabled ? null : this.props.onClick}
+//        style={this.props.disabled ? styles.disabledTab : (
+//          this.props.isActive ? styles.activeTab : styles.tab
+//        )}
+//      >
+//        {this.props.children}
+//      </div>
+//    )
+//  }
+//})
+//
+//const TabPanels = React.createClass({
+//  render() {
+//    return (
+//      <div style={styles.tabPanels}>
+//        {this.props.children[this.props.activeIndex]}
+//      </div>
+//    )
+//  }
+//})
+//
+//const TabPanel = React.createClass({
+//  render() {
+//    return <div>{this.props.children}</div>
+//  }
+//})
+//
+//const Tabs = React.createClass({
+//  getInitialState() {
+//    return {
+//      activeIndex: 0
+//    }
+//  },
+//
+//  render() {
+//    const children = React.Children.map(this.props.children, (child, index) => {
+//      if (child.type === TabPanels) {
+//        return React.cloneElement(child, {
+//          activeIndex: this.state.activeIndex
+//        })
+//      } else if (child.type === TabList) {
+//        return React.cloneElement(child, {
+//          activeIndex: this.state.activeIndex,
+//          onActivate: (activeIndex) => this.setState({ activeIndex })
+//        })
+//      } else {
+//        return child
+//      }
+//    })
+//
+//    return <div>{children}</div>
+//  }
+//})
+//
 //const App = React.createClass({
-  //render () {
-    //return (
-      //<div>
-        //<Tabs>
-          //<TabList>
-            //<Tab>Tacos</Tab>
-            //<Tab disabled>Burritos</Tab>
-            //<Tab>Coconut Korma</Tab>
-          //</TabList>
-
-          //<TabPanels>
-            //<TabPanel>
-              //<p>Tacos are delicious</p>
-            //</TabPanel>
-            //<TabPanel>
-              //<p>Sometimes a burrito is what you really need.</p>
-            //</TabPanel>
-            //<TabPanel>
-              //<p>Might be your best option.</p>
-            //</TabPanel>
-          //</TabPanels>
-        //</Tabs>
-      //</div>
-    //);
-  //}
-//});
-
+//  render() {
+//    return (
+//      <div>
+//        <Tabs>
+//          <TabList>
+//            <Tab>Tacos</Tab>
+//            <Tab disabled>Burritos</Tab>
+//            <Tab>Coconut Korma</Tab>
+//          </TabList>
+//
+//          <TabPanels>
+//            <TabPanel>
+//              <p>Tacos are delicious</p>
+//            </TabPanel>
+//            <TabPanel>
+//              <p>Sometimes a burrito is what you really need.</p>
+//            </TabPanel>
+//            <TabPanel>
+//              <p>Might be your best option.</p>
+//            </TabPanel>
+//          </TabPanels>
+//        </Tabs>
+//      </div>
+//    )
+//  }
+//})
+//
+//render(<App/>, document.getElementById('app'))
 
 ////////////////////////////////////////////////////////////////////////////////
 // Oh but you really loved the old tabs yeah?
+
 //const DataTabs = React.createClass({
-  //getDefaultProps () {
-    //return {
-      //disabled: []
-    //}
-  //},
-  //render () {
-    //return (
-      //<Tabs>
-        //<TabList>
-          //{this.props.data.map((item, index) => (
-            //<Tab key={item.label} disabled={this.props.disabled.indexOf(index) !== -1}>
-              //{item.label}
-            //</Tab>
-          //))}
-        //</TabList>
-
-        //<TabPanels>
-          //{this.props.data.map((item) => (
-            //<TabPanel key={item.label}>{item.description}</TabPanel>
-          //))}
-        //</TabPanels>
-      //</Tabs>
-    //);
-  //}
-//});
-
+//  getDefaultProps () {
+//    return {
+//      disabled: []
+//    }
+//  },
+//  render() {
+//    return (
+//      <Tabs>
+//        <TabList>
+//          {this.props.data.map((item, index) => (
+//            <Tab key={item.label} disabled={this.props.disabled.indexOf(index) !== -1}>
+//              {item.label}
+//            </Tab>
+//          ))}
+//        </TabList>
+//
+//        <TabPanels>
+//          {this.props.data.map((item) => (
+//            <TabPanel key={item.label}>{item.description}</TabPanel>
+//          ))}
+//        </TabPanels>
+//      </Tabs>
+//    )
+//  }
+//})
+//
 //const App = React.createClass({
-
-  //render () {
-    //const tabData = [
-      //{
-        //label: 'Tacos',
-        //description: <p>Tacos are delicious</p>
-      //},
-      //{
-        //label: 'Burritos',
-        //description: <p>Sometimes a burrito is what you really need.</p>
-      //},
-      //{
-        //label: 'Coconut Korma',
-        //description: <p>Might be your best option.</p>
-      //},
-
-    //]
-
-    //return (
-      //<div>
-        //<DataTabs data={tabData}/>
-      //</div>
-    //);
-  //}
-//});
-
+//
+//  render() {
+//    const tabData = [
+//      {
+//        label: 'Tacos',
+//        description: <p>Tacos are delicious</p>
+//      },
+//      {
+//        label: 'Burritos',
+//        description: <p>Sometimes a burrito is what you really need.</p>
+//      },
+//      {
+//        label: 'Coconut Korma',
+//        description: <p>Might be your best option.</p>
+//      }
+//    ]
+//
+//    return (
+//      <div>
+//        <DataTabs data={tabData}/>
+//      </div>
+//    )
+//  }
+//})
+//
 //render(<App/>, document.getElementById('app'))
 
 ////////////////////////////////////////////////////////////////////////////////
