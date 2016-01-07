1a36ba8d9013f8a2fb9e980676306eb4de4eed95
Michael Jackson <mjijackson@gmail.com>
2016-01-07 11:57:02 -0800

    Better state name

--- subjects/05-imperative-to-declarative/solution.js
+++ subjects/05-imperative-to-declarative/solution.js
@@ -59,16 +59,16 @@ const App = React.createClass({
 
   getInitialState() {
     return {
-      modalIsOpen: false
+      isModalOpen: false
     }
   },
 
   openModal() {
-    this.setState({ modalIsOpen: true })
+    this.setState({ isModalOpen: true })
   },
 
   closeModal() {
-    this.setState({ modalIsOpen: false })
+    this.setState({ isModalOpen: false })
   },
 
   render() {
@@ -81,7 +81,7 @@ const App = React.createClass({
           onClick={this.openModal}
         >open modal</button>
 
-        <Modal isOpen={this.state.modalIsOpen} title="Declarative is better">
+        <Modal isOpen={this.state.isModalOpen} title="Declarative is better">
           <p>Calling methods on instances is a FLOW not a STOCK!</p>
           <p>It’s the dynamic process, not the static program in text space.</p>
           <p>You have to experience it over time, rather than in snapshots of state.</p>
