8468961404ffe244f0ae85c917d372b32efc8037
Ryan Florence <rpflorence@gmail.com>
2016-01-12 10:06:54 -0700

    add dispatcher logging

--- subjects/14-flux/exercise/AppDispatcher.js
+++ subjects/14-flux/exercise/AppDispatcher.js
@@ -1,9 +1,11 @@
+/* eslint no-console: 0 */
 import { Dispatcher } from 'flux'
 import { PayloadSources } from './Constants'
 
 const AppDispatcher = new Dispatcher
-  
+
 export function dispatchViewAction(action) {
+  console.log('dispatching view action', action)
   AppDispatcher.dispatch({
     source: PayloadSources.VIEW_ACTION,
     action: action
@@ -11,6 +13,7 @@ export function dispatchViewAction(action) {
 }
 
 export function dispatchServerAction(action) {
+  console.log('dispatching server action', action)
   AppDispatcher.dispatch({
     source: PayloadSources.SERVER_ACTION,
     action: action
