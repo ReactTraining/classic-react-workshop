dc82256dd1f2008e75aa7af180f6797dee3a1138
Michael Jackson <mjijackson@gmail.com>
2016-01-21 21:14:12 +0000

    Update styles

--- subjects/06-compound-components/lib/styles.js
+++ subjects/06-compound-components/lib/styles.js
@@ -1,24 +1,25 @@
-export var tab = {
+export const tabs = {}
+
+export const tab = {
   display: 'inline-block',
   padding: 10,
   margin: 10,
   borderBottom: '4px solid',
   borderBottomColor: '#ccc',
   cursor: 'pointer'
-};
+}
 
-export var activeTab = {
+export const activeTab = {
   ...tab,
   borderBottomColor: '#000'
-};
+}
 
-export var disabledTab = {
+export const disabledTab = {
   ...tab,
   opacity: 0.25,
   cursor: 'default'
-};
+}
 
-export var tabPanels = {
+export const tabPanels = {
   padding: 10
-};
-
+}
