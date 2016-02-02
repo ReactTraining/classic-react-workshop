64fb226c0d5bb97639c12c45c3e496368edee3f6
Michael Jackson <mjijackson@gmail.com>
2016-02-02 11:45:31 -0800

    Fix broken test

--- subjects/02-rendering/tests.js
+++ subjects/02-rendering/tests.js
@@ -8,7 +8,7 @@ export function run() {
   assert(!!html.match(/burrito/), 'render burrito')
   assert(!!html.match(/tacos/), 'render tacos')
   assert(!!html.match(/tostada/), 'render tostada')
-  assert(!html.match(/hush puppies/), 'filter out hush puppies')
+  assert(!html.match(/mushy peas/), 'only show mexican food')
   assert(html.indexOf('burrito') < html.indexOf('tacos'), 'burrito rendered first')
   assert(html.indexOf('tacos') < html.indexOf('tostada'), 'tacos rendered second')
 }
