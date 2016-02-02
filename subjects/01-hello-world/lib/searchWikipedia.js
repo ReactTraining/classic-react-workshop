277dba29295c4ddd1a49d65b50b8ae43e3914b57
Ryan Florence <rpflorence@gmail.com>
2016-02-01 23:32:49 -0800

    new lecture

---
+++ subjects/01-hello-world/lib/searchWikipedia.js
@@ -0,0 +1,21 @@
+import jsonp from 'jsonp'
+
+const API = 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json'
+
+export function search(term, cb) {
+  jsonp(`${API}&search=${term}`, (err, data) => {
+    if (err) {
+      cb(err)
+    } else {
+      const [ searchTerm, titles, descriptions, urls ] = data
+      cb(null, titles.sort().map((title, index) => {
+        return {
+          title,
+          description: descriptions[index],
+          url: urls[index]
+        }
+      }))
+    }
+  })
+}
+
