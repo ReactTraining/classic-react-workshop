7dd6528b9abcf73b81251703eb43511c736a076e
Michael Jackson <mjijackson@gmail.com>
2016-02-05 14:01:34 -0800

    Update animation lecture

--- subjects/11-animation/lecture.js
+++ subjects/11-animation/lecture.js
@@ -1,56 +1,111 @@
 import React from 'react'
-import { render } from 'react-dom'
+import { render, findDOMNode } from 'react-dom'
 import { Motion, StaggeredMotion, spring, presets } from 'react-motion'
+import $ from 'jquery'
 
 require('./styles')
 
 ///////////////////////////////////////////////////////////////////////////////
-// Let's create a simple toggle switch that animates back and forth when we
-// click it. Use a spring to make the transition feel natural.
+// Let's create a simple toggle switch that moves back and forth.
 
 const ToggleSwitch = React.createClass({
-
   getInitialState() {
     return {
       isActive: false
     }
   },
-
   toggle() {
     this.setState({
       isActive: !this.state.isActive
     })
   },
-  
   render() {
     const x = this.state.isActive ? 400 : 0
 
     return (
-      <Motion style={{ x: spring(x) }}>
-        {style =>
-          <div id="switch1" className="toggle-switch" onClick={this.toggle}>
-            <div className="toggle-switch-knob" style={{
-              WebkitTransform: `translate3d(${style.x}px,0,0)`,
-              transform: `translate3d(${style.x}px,0,0)`
-            }}/>
-          </div>
-        }
-      </Motion>
+      <div id="switch1" className="toggle-switch" onClick={this.toggle}>
+        <div className="toggle-switch-knob" style={{ left: x }}/>
+      </div>
     )
   }
-
 })
 
 render(<ToggleSwitch/>, document.getElementById('app'))
 
-////////////////////////////////////////////////////////////////////////////////
-// Update the stiffness and damping of the spring to see how they modify the
-// animation. Try using preset values when you're getting started. Also, try
-// the Spring Parameters Chooser demo.
+///////////////////////////////////////////////////////////////////////////////
+// We can integrate with other DOM animation libraries by doing imperative work
+// in the usual lifecycle methods. But our render method isn't as obvious. :/
+
+//const ToggleSwitch = React.createClass({
+//  getInitialState() {
+//    return {
+//      isActive: false
+//    }
+//  },
+//  toggle() {
+//    this.setState({
+//      isActive: !this.state.isActive
+//    })
+//  },
+//  componentDidUpdate() {
+//    const x = this.state.isActive ? 400 : 0
+//
+//    $(findDOMNode(this))
+//      .find('.toggle-switch-knob')
+//      .animate({ left: x }, 'swing')
+//  },
+//  render() {
+//    return (
+//      <div id="switch1" className="toggle-switch" onClick={this.toggle}>
+//        <div className="toggle-switch-knob"/>
+//      </div>
+//    )
+//  }
+//})
+//
+//render(<ToggleSwitch/>, document.getElementById('app'))
+
+///////////////////////////////////////////////////////////////////////////////
+// react-motion is an interesting library that gives us a way to do animation
+// declaratively! Also, it uses springs to make animation feel more natural.
+// Try different stiffness and damping values to see how they change the feel
+// of the spring. The Spring Parameters Chooser demo lets you play with
+// different stiffness/damping values.
+//
+// http://chenglou.me/react-motion/demos/demo5-spring-parameters-chooser/
+
+//const ToggleSwitch = React.createClass({
+//  getInitialState() {
+//    return {
+//      isActive: false
+//    }
+//  },
+//  toggle() {
+//    this.setState({
+//      isActive: !this.state.isActive
+//    })
+//  },
+//  render() {
+//    const x = this.state.isActive ? 400 : 0
+//
+//    return (
+//      <Motion style={{ x: spring(x, { stiffness: 170, damping: 26 }) }}>
+//        {style =>
+//          <div id="switch1" className="toggle-switch" onClick={this.toggle}>
+//            <div className="toggle-switch-knob" style={{ left: style.x }}/>
+//          </div>
+//        }
+//      </Motion>
+//    )
+//  }
+//})
+//
+//render(<ToggleSwitch/>, document.getElementById('app'))
 
 ///////////////////////////////////////////////////////////////////////////////
 // Use the same <Motion> to animate several springs at once. All animations
-// should run concurrently and finish at roughly the same time.
+// should run concurrently and finish at roughly the same time. They're
+// completely coordinated!
 
 //const ToggleSwitch = React.createClass({
 //
@@ -76,22 +131,13 @@ render(<ToggleSwitch/>, document.getElementById('app'))
 //        {style => (
 //          <div>
 //            <div id="switch1" className="toggle-switch" onClick={this.toggle}>
-//              <div className="toggle-switch-knob" style={{
-//                WebkitTransform: `translate3d(${style.x1}px,0,0)`,
-//                transform: `translate3d(${style.x1}px,0,0)`
-//              }}/>
+//              <div className="toggle-switch-knob" style={{ left: style.x1 }}/>
 //            </div>
 //            <div id="switch2" className="toggle-switch" onClick={this.toggle}>
-//              <div className="toggle-switch-knob" style={{
-//                WebkitTransform: `translate3d(${style.x2}px,0,0)`,
-//                transform: `translate3d(${style.x2}px,0,0)`
-//              }}/>
+//              <div className="toggle-switch-knob" style={{ left: style.x2 }}/>
 //            </div>
 //            <div id="switch3" className="toggle-switch" onClick={this.toggle}>
-//              <div className="toggle-switch-knob" style={{
-//                WebkitTransform: `translate3d(${style.x3}px,0,0)`,
-//                transform: `translate3d(${style.x3}px,0,0)`
-//              }}/>
+//              <div className="toggle-switch-knob" style={{ left: style.x3 }}/>
 //            </div>
 //          </div>
 //        )}
@@ -109,19 +155,16 @@ render(<ToggleSwitch/>, document.getElementById('app'))
 // known as onion skinning.
 
 //const ToggleSwitch = React.createClass({
-//
 //  getInitialState() {
 //    return {
 //      isActive: false
 //    }
 //  },
-//
 //  toggle() {
 //    this.setState({
 //      isActive: !this.state.isActive
 //    })
 //  },
-//
 //  render() {
 //    const x = this.state.isActive ? 400 : 0
 //
@@ -136,8 +179,7 @@ render(<ToggleSwitch/>, document.getElementById('app'))
 //          <div id="switch1" className="toggle-switch" onClick={this.toggle}>
 //            {styles.map((style, i) => (
 //              <div key={i} className="toggle-switch-knob" style={{
-//                WebkitTransform: `translate3d(${style.x}px,0,0)`,
-//                transform: `translate3d(${style.x}px,0,0)`,
+//                left: style.x,
 //                opacity: 1 - (0.2 * i)
 //              }}/>
 //            ))}
