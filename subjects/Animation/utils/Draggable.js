74416f7c6ab029b3ab51471d51df4e648eb9ae88
Michael Jackson <mjijackson@gmail.com>
2016-03-24 13:33:35 -0700

    Update animation exercise/solution

---
+++ subjects/Animation/utils/Draggable.js
@@ -0,0 +1,67 @@
+import React, { PropTypes } from 'react'
+
+const Draggable = React.createClass({
+  propTypes: {
+    component: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.func
+    ]).isRequired,
+    onDragStart: PropTypes.func,
+    onDrag: PropTypes.func,
+    onDrop: PropTypes.func
+  },
+
+  getDefaultProps() {
+    return {
+      component: 'div'
+    }
+  },
+
+  componentDidMount() {
+    this.isDragging = false
+    document.addEventListener('mouseup', this.handleMouseUp)
+    document.addEventListener('mousemove', this.handleMouseMove)
+  },
+
+  componentWillUnmount() {
+    document.removeEventListener('mousemove', this.handleMouseMove)
+    document.removeEventListener('mouseup', this.handleMouseUp)
+  },
+
+  handleMouseDown(event) {
+    if (!this.isDragging) {
+      this.isDragging = true
+
+      // Prevent Chrome from displaying a text cursor
+      event.preventDefault()
+
+      if (this.props.onDragStart)
+        this.props.onDragStart(event)
+    }
+  },
+
+  handleMouseMove(event) {
+    if (this.isDragging && this.props.onDrag)
+      this.props.onDrag(event)
+  },
+
+  handleMouseUp(event) {
+    if (this.isDragging) {
+      this.isDragging = false
+      
+      if (this.props.onDrop)
+        this.props.onDrop(event)
+    }
+  },
+
+  render() {
+    const { component, ...otherProps } = this.props
+
+    return React.createElement(component, {
+      ...otherProps,
+      onMouseDown: this.handleMouseDown
+    })
+  }
+})
+
+export default Draggable
