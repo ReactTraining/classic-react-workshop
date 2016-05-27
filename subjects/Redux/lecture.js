913ceebecf0987e46fb9e1667e1b016bdaef2c81
Michael Jackson <mjijackson@gmail.com>
2016-05-27 13:25:04 -0500

    Add Redux lecture

--- subjects/Redux/lecture.js
+++ subjects/Redux/lecture.js
@@ -1,17 +1,33 @@
 /*
- * 0. app with contacts in state.
- *    - What state is there?
- *    - When does it change?
- * 1. move creating contacts into redux
- *    - state is defined in reducers
- *    - immutable changes
- *    - changes to state modeled as actions
- *
- * 2. introduce middleware (logger)
- * 3. load contacts from the server (thunk)
- * 4. save the contact to the server
- * 5. exercise: delete contacts
- * 6. exercise: edit contacts
- */
+- Flux is an architecture, not a framework
+  - DO NOT START BUILDING STUFF WITH FLUX WHEN YOU'RE FIRST GETTING STARTED WITH REACT
+  - It can be difficult to understand why the patterns in Flux are useful if you haven't
+    already tried to solve problems w/out Flux
+  - You'll most likely hate Flux unless you're already fighting with your current JS
+    framework. If you're not, stick with what's working for you
 
-import './lecture/index'
+- Flux is good at:
+  - Making it easy to reason about changes to state
+
+- Remember our 2 questions:
+  - What state is there?
+  - When does it change?
+
+Open Redux.png
+
+- Views
+  - React components (see components)
+  - Create actions (see actions)
+
+- Actions
+  - Create "actions" with meaningful names (e.g. "load contacts", "delete contact").
+    These are the verbs. Ask yourself, "what actions can the user take?"
+  - Send actions through the dispatcher
+  - Possibly trigger API requests (side effect)
+
+- Store
+  - Synchronous dispatch of actions to ALL registered listeners (stores)
+
+- Reducers
+  - Compute new state values
+*/
