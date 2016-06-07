cea2fdbf8168ccc80ef4846da76780083c1446fc
Michael Jackson <mjijackson@gmail.com>
2016-06-07 14:30:52 -0700

    Guard against no tabs being in the page

--- subjects/Components/tests.js
+++ subjects/Components/tests.js
@@ -17,22 +17,22 @@ export function run(component) {
   assert(!!html.match(/Brazil/), 'render Brazil tab')
   assert(!!html.match(/Russia/), 'render Russia tab')
   assert(
-    tabs[0].style.borderBottomColor === borderFixture.style.borderBottomColor,
+    tabs[0] && tabs[0].style.borderBottomColor === borderFixture.style.borderBottomColor,
     'first tab is active'
   )
   assert(
-    tabs[1].style.borderBottomColor !== borderFixture.style.borderBottomColor,
+    tabs[1] && tabs[1].style.borderBottomColor !== borderFixture.style.borderBottomColor,
     'second tab is inactive'
   )
 
   console.log('after clicking the second tab...')
   Simulate.click(tabs[1])
   assert(
-    tabs[1].style.borderBottomColor === borderFixture.style.borderBottomColor,
+    tabs[1] && tabs[1].style.borderBottomColor === borderFixture.style.borderBottomColor,
     'second tab is active'
   )
   assert(
-    tabs[0].style.borderBottomColor !== borderFixture.style.borderBottomColor,
+    tabs[0] && tabs[0].style.borderBottomColor !== borderFixture.style.borderBottomColor,
     'first tab is inactive'
   )
   assert(
