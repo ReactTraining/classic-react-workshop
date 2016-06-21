3fd029ece144a52983247265d8ec2d4a6a1345b2
Michael Jackson <mjijackson@gmail.com>
2016-06-20 22:29:57 -0700

    Update lecture

--- subjects/HigherOrderComponents/lecture.js
+++ subjects/HigherOrderComponents/lecture.js
@@ -1,6 +1,6 @@
-import createMediaListener from './lib/createMediaListener'
 import React from 'react'
 import { render } from 'react-dom'
+import createMediaListener from './lib/createMediaListener'
 
 const media = createMediaListener({
   big: '(min-width : 1000px)',
@@ -8,7 +8,6 @@ const media = createMediaListener({
 })
 
 const App = React.createClass({
-
   getInitialState() {
     return {
       media: media.getState()
@@ -27,15 +26,15 @@ const App = React.createClass({
 
   render() {
     const { media } = this.state
+
     return media.big ? (
       <h1>Hey, this is a big screen</h1>
     ) : media.tiny ? (
       <h6>tiny tiny tiny</h6>
     ) : (
-      <h3>Meh ...</h3>
+      <h3>Meh...</h3>
     )
   }
-
 })
 
 render(<App/>, document.getElementById('app'))
@@ -46,57 +45,54 @@ render(<App/>, document.getElementById('app'))
 // and returns a new component renders the `Component` with some extra props.
 
 //const mediaComponent = (Component, mediaQueries) => {
-  //const media = createMediaListener(mediaQueries)
-
-  //return React.createClass({
-    //getInitialState() {
-      //return {
-        //media: media.getState()
-      //}
-    //},
-
-    //componentDidMount() {
-      //media.listen((media) => {
-        //this.setState({ media })
-      //})
-    //},
-
-    //componentWillUnmount() {
-      //media.dispose()
-    //},
-
-    //render() {
-      //return <Component {...this.props} media={this.state.media}/>
-    //}
-  //})
+//  const media = createMediaListener(mediaQueries)
+//
+//  return React.createClass({
+//    getInitialState() {
+//      return {
+//        media: media.getState()
+//      }
+//    },
+//
+//    componentDidMount() {
+//      media.listen((media) => {
+//        this.setState({ media })
+//      })
+//    },
+//
+//    componentWillUnmount() {
+//      media.dispose()
+//    },
+//
+//    render() {
+//      return <Component {...this.props} media={this.state.media}/>
+//    }
+//  })
 //}
-
+//
 //const App = React.createClass({
-
-  //propTypes: {
-    //media: React.PropTypes.shape({
-      //big: React.PropTypes.bool,
-      //tiny: React.PropTypes.bool
-    //})
-  //},
-
-  //render() {
-    //const { media } = this.props
-    //return media.big ? (
-      //<h1>Hey, this is a big screen</h1>
-    //) : media.tiny ? (
-      //<h6>tiny tiny tiny</h6>
-    //) : (
-      //<h3>Meh ...</h3>
-    //)
-  //}
-
+//  propTypes: {
+//    media: React.PropTypes.shape({
+//      big: React.PropTypes.bool,
+//      tiny: React.PropTypes.bool
+//    })
+//  },
+//
+//  render() {
+//    const { media } = this.props
+//    return media.big ? (
+//      <h1>Hey, this is a big screen</h1>
+//    ) : media.tiny ? (
+//      <h6>tiny tiny tiny</h6>
+//    ) : (
+//      <h3>Meh ...</h3>
+//    )
+//  }
 //})
-
+//
 //const AppWithMedia = mediaComponent(App, {
-  //big: '(min-width : 1000px)',
-  //tiny: '(max-width: 400px)'
+//  big: '(min-width : 1000px)',
+//  tiny: '(max-width: 400px)'
 //})
-
+//
 //render(<AppWithMedia/>, document.getElementById('app'))
-
