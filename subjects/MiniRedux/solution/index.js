06cd7ed00609ba7b1fbf5343c9acb27754e7115e
Michael Jackson <mjijackson@gmail.com>
2016-06-21 14:31:28 -0700

    Update instructions

--- subjects/MiniRedux/solution/index.js
+++ subjects/MiniRedux/solution/index.js
@@ -1,16 +1,15 @@
 ////////////////////////////////////////////////////////////////////////////////
 // Exercise
 //
-// 1. Implement Provider to make the store accessible on context to the rest
+// 1. Implement <Provider> to make the store accessible on context to the rest
 //    of the components rendered below it
-//
 // 2. Implement `connect` it should:
-//    1. return a function that takes a component
-//    2. The new function should return a new component that wraps the component
-//       passed to it.
-//    3. The new component, when rendered, will pass state from
+//   a) Return a function that takes a component
+//   b) The new function should return a new component that wraps the component
+//      passed to it
+//   c) The new component, when rendered, will pass state from
 //      the store as props to your App component. You'll use the function
-//      passed to `connect` to map store state to component props.
+//      passed to `connect` to map store state to component props
 //
 // Got extra time?
 //
@@ -19,10 +18,10 @@
 // 5. Handle creating contacts with `dispatch`
 
 import React from 'react'
-import App from './components/App'
 import { render } from 'react-dom'
 import createStore from './mini-redux/createStore'
 import Provider from './mini-redux/Provider'
+import App from './components/App'
 
 const store = createStore((state = 0, action) => {
   if (action.type === 'INCREMENT') {
