11588ac885724c871ae1884272b004c446faef9e
Michael Jackson <mjijackson@gmail.com>
2016-06-28 11:46:19 -0700

    Remove Redux
    Watch the egghead videos instead :P

--- subjects/MiniRedux/exercise/mini-redux/Provider.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Provider = React.createClass({
-  render() {
-    return <div>{this.props.children}</div>
-  }
-})
-
-export default Provider
--- subjects/MiniRedux/exercise/mini-redux/connect.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function connect(mapStateToProps) {
-  return function (Component) {
-    return Component
-  }
-}
