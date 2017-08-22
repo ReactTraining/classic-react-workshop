985fd3a72f505f6972ebf0e515637225157542ab
Pedro Canterini <pcanterini@bleacherreport.com>
2017-08-21 18:43:25 -0700

    improve error handling

--- subjects/RenderProps/utils/getAddressFromCoords.js
+++ subjects/RenderProps/utils/getAddressFromCoords.js
@@ -5,9 +5,12 @@ const GoogleMapsAPI = 'https://maps.googleapis.com/maps/api'
 const unthrottledGetAddressFromCoords = (latitude, longitude) => {
   const url = `${GoogleMapsAPI}/geocode/json?latlng=${latitude},${longitude}`
 
-  return fetch(url).then(res => res.json()).then(json => {
-    return json.results[0].formatted_address
-  })
+  return fetch(url)
+    .then(res => res.json())
+    .then(json => json.results[0].formatted_address)
+    .catch(error => {
+      throw error
+    })
 }
 
 // Throttle requests to once per second
