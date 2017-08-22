bd3e94dfbf41183c30062dc86bfbf90746a67bd9
Pedro Canterini <pcanterini@bleacherreport.com>
2017-08-21 18:19:28 -0700

    throttle geo address requests

--- subjects/RenderProps/utils/getAddressFromCoords.js
+++ subjects/RenderProps/utils/getAddressFromCoords.js
@@ -1,11 +1,16 @@
+import debounce from 'debounce-promise'
+
 const GoogleMapsAPI = 'https://maps.googleapis.com/maps/api'
 
-const getAddressFromCoords = (latitude, longitude) => {
+const unthrottledGetAddressFromCoords = (latitude, longitude) => {
   const url = `${GoogleMapsAPI}/geocode/json?latlng=${latitude},${longitude}`
 
   return fetch(url).then(res => res.json()).then(json => {
     return json.results[0].formatted_address
   })
 }
+
+// Throttle requests to once per second
+const getAddressFromCoords = debounce(unthrottledGetAddressFromCoords, 1000)
 
 export default getAddressFromCoords
