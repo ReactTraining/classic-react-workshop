c1b93341ad6d88c7851bf1823ca248a531b2d386
MICHAEL JACKSON <mjijackson@gmail.com>
2017-12-08 12:55:57 -0800

    Throttle getAddressFromCoords

--- subjects/RenderProps/utils/getAddressFromCoords.js
+++ subjects/RenderProps/utils/getAddressFromCoords.js
@@ -1,21 +1,35 @@
-import debounce from "debounce-promise"
-
 const GoogleMapsAPI = "https://maps.googleapis.com/maps/api"
 
-const unthrottledGetAddressFromCoords = (latitude, longitude) => {
-  const url = `${GoogleMapsAPI}/geocode/json?latlng=${latitude},${
-    longitude
-  }`
+const getAddressFromCoords = (latitude, longitude) => {
+  const url = `${GoogleMapsAPI}/geocode/json?latlng=${latitude},${longitude}`
 
   return fetch(url)
     .then(res => res.json())
     .then(json => json.results[0].formatted_address)
 }
 
-// Throttle requests to once per second
-const getAddressFromCoords = debounce(
-  unthrottledGetAddressFromCoords,
-  1000
-)
+const maxCallsPerMinute = 20
+let lastCallTime = 0
+let lastWarningTime = 0
+let promise = Promise.resolve()
 
-export default getAddressFromCoords
+const throttledGetAddressFromCoords = (latitude, longitude) => {
+  const currentTime = Date.now()
+  const requiredWaitTime = 60000 / maxCallsPerMinute
+
+  if (lastCallTime + requiredWaitTime < currentTime) {
+    lastCallTime = currentTime
+    promise = getAddressFromCoords(latitude, longitude)
+  } else if (currentTime - lastWarningTime > 5000) {
+    lastWarningTime = currentTime
+    window.alert(
+      "It looks like you're calling getAddressFromCoords many times " +
+        "quickly in a loop. Take a closer look at the componentDidUpdate " +
+        "function in <GeoAddress>..."
+    )
+  }
+
+  return promise
+}
+
+export default throttledGetAddressFromCoords
