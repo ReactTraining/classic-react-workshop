e98faf49e3df97461cd5daaa3d4e003e1c95533e
MICHAEL JACKSON <mj@mjackson.me>
2018-02-21 23:52:14 -0800

    Moving things around

--- scripts/build.js
+++ scripts/build.js
@@ -5,7 +5,57 @@ const ReactDOMServer = require("react-dom/server");
 
 const e = React.createElement;
 
-function createMarkup(mainBundle) {
+function fileExists(file) {
+  try {
+    const stats = fs.statSync(file);
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
+
+function leftPad(s, length) {
+  s = "" + s;
+
+  while (s.length < length) {
+    s = "0" + s;
+  }
+
+  return s;
+}
+
+function createIndexMarkup(rows) {
+  return ReactDOMServer.renderToStaticMarkup(
+    e(
+      "html",
+      null,
+      e(
+        "head",
+        null,
+        e("link", { rel: "stylesheet", href: "/shared.css" })
+      ),
+      e(
+        "body",
+        null,
+        e(
+          "div",
+          { className: "index" },
+          e(
+            "table",
+            {
+              className: "index-subjectsTable",
+              cellSpacing: 0,
+              cellPadding: 0
+            },
+            e("tbody", null, rows)
+          )
+        )
+      )
+    )
+  );
+}
+
+function createSubjectMarkup(bundle) {
   return ReactDOMServer.renderToStaticMarkup(
     e(
       "html",
@@ -20,115 +70,113 @@ function createMarkup(mainBundle) {
         null,
         e("div", { id: "app" }),
         e("script", { src: "/__build__/shared.js" }),
-        e("script", { src: "/__build__/" + mainBundle + ".js" })
+        e("script", { src: "/__build__/" + bundle + ".js" })
       )
     )
   );
 }
 
-const RootDir = path.resolve(__dirname, "..");
-const SubjectsDir = path.join(RootDir, "subjects");
+const rootDir = path.resolve(__dirname, "..");
+const subjectsDir = path.join(rootDir, "subjects");
 
-const Subjects = {
-  HelloWorld: "Hello World",
-  Rendering: "Rendering",
-  Components: "Components",
-  PropsVsState: "Props vs. State",
-  Forms: "Forms",
-  Testing: "Testing",
-  ImperativeToDeclarative: "Imperative to Declarative",
-  CompoundComponents: "Compound Components",
-  Context: "Context",
-  HigherOrderComponents: "Higher Order Components",
-  RenderProps: "Render Props",
-  RenderOptimizations: "Render Optimizations",
-  TweenState: "TweenState",
-  Animation: "Animation",
-  Routing: "Routing",
-  ServerRendering: "Server Rendering",
-  JSONTable: "JSON Table",
-  Select: "Select",
-  Slider: "Slider",
-  ChatApp: "Chat App",
-  Cursors: "Cursors",
-  MigratingToReact: "Migrating to React",
-  Redux: "Redux",
-  MiniRedux: "Mini Redux",
-  MiniRouter: "Mini Router"
-};
+const subjects = [
+  "Hello World",
+  "Rendering",
+  "Components",
+  "Props and State",
+  "Forms",
+  "Testing",
+  "Imperative to Declarative",
+  "Compound Components",
+  "Context",
+  "Higher-order Components",
+  "Render Props",
+  "Routing",
+  "Animation",
+  "Transitions",
+  "Render Optimizations",
+  "Server Rendering",
+  "Redux",
+  "Immutability",
+  "Migrating to React"
+];
 
-const SubjectDirNames = Object.keys(Subjects);
+const exercises = [
+  "Chat App",
+  "JSON Table",
+  "Mini Redux",
+  "Mini Router",
+  "Select",
+  "Slider"
+];
 
-const markup = ReactDOMServer.renderToStaticMarkup(
-  e(
-    "html",
-    null,
-    e(
-      "head",
-      null,
-      e("link", { rel: "stylesheet", href: "/shared.css" })
-    ),
-    e(
-      "body",
-      { id: "index" },
+const indexRows = [];
+
+subjects.forEach(function(subject, index) {
+  const n = leftPad(index, 2);
+  const dir = n + " " + subject;
+
+  const cells = [
+    e("td", { className: "index-subjectNumber", key: 0 }, n)
+  ];
+
+  if (fileExists(path.join(subjectsDir, dir, "lecture.js"))) {
+    fs.writeFileSync(
+      path.join(subjectsDir, dir, "lecture.html"),
+      createSubjectMarkup(dir + "-lecture")
+    );
+
+    cells.push(
       e(
-        "table",
-        { cellSpacing: 0, cellPadding: 0 },
-        e(
-          "tbody",
-          null,
-          SubjectDirNames.map(function(dir, index) {
-            return e(
-              "tr",
-              { key: dir, className: index % 2 ? "odd" : "even" },
-              e(
-                "td",
-                { className: "lecture-link" },
-                e(
-                  "a",
-                  { href: "/" + dir + "/lecture.html" },
-                  Subjects[dir]
-                )
-              ),
-              e(
-                "td",
-                { className: "exercise-link" },
-                e(
-                  "a",
-                  { href: "/" + dir + "/exercise.html" },
-                  "exercise"
-                )
-              ),
-              e(
-                "td",
-                { className: "solution-link" },
-                e(
-                  "a",
-                  { href: "/" + dir + "/solution.html" },
-                  "solution"
-                )
-              )
-            );
-          })
-        )
+        "td",
+        { className: "index-lectureCell", key: 1 },
+        e("a", { href: "/" + dir + "/lecture.html" }, subject)
       )
-    )
-  )
-);
+    );
+  } else {
+    cells.push(
+      e("td", { className: "index-lectureCell", key: 1 }, subject)
+    );
+  }
 
-fs.writeFileSync(path.join(SubjectsDir, "index.html"), markup);
+  if (fileExists(path.join(subjectsDir, dir, "exercise.js"))) {
+    fs.writeFileSync(
+      path.join(subjectsDir, dir, "exercise.html"),
+      createSubjectMarkup(dir + "-exercise")
+    );
 
-SubjectDirNames.forEach(function(dir) {
-  fs.writeFileSync(
-    path.join(SubjectsDir, dir, "lecture.html"),
-    createMarkup(dir + "-lecture")
-  );
-  fs.writeFileSync(
-    path.join(SubjectsDir, dir, "exercise.html"),
-    createMarkup(dir + "-exercise")
-  );
-  fs.writeFileSync(
-    path.join(SubjectsDir, dir, "solution.html"),
-    createMarkup(dir + "-solution")
-  );
+    cells.push(
+      e(
+        "td",
+        { className: "index-exerciseCell", key: 2 },
+        e("a", { href: "/" + dir + "/exercise.html" }, "exercise")
+      )
+    );
+  } else {
+    cells.push(e("td", { className: "index-exerciseCell", key: 2 }));
+  }
+
+  if (fileExists(path.join(subjectsDir, dir, "solution.js"))) {
+    fs.writeFileSync(
+      path.join(subjectsDir, dir, "solution.html"),
+      createSubjectMarkup(dir + "-solution")
+    );
+
+    cells.push(
+      e(
+        "td",
+        { className: "index-solutionCell", key: 3 },
+        e("a", { href: "/" + dir + "/solution.html" }, "solution")
+      )
+    );
+  } else {
+    cells.push(e("td", { className: "index-solutionCell", key: 3 }));
+  }
+
+  indexRows.push(e("tr", { key: dir }, cells));
 });
+
+fs.writeFileSync(
+  path.join(subjectsDir, "index.html"),
+  createIndexMarkup(indexRows)
+);
--- subjects/Flux/exercise/actions/ServerActionCreators.js
+++
@@ -1,9 +0,0 @@
-import { ActionTypes } from "../Constants";
-import { dispatchServerAction } from "../AppDispatcher";
-
-export function contactsWereLoaded(contacts) {
-  dispatchServerAction({
-    type: ActionTypes.CONTACTS_WERE_LOADED,
-    contacts
-  });
-}
