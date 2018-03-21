1353335aeedaee8744a66f50a8628c15a444d0d0
Michael Jackson <mj@mjackson.me>
2018-03-21 16:01:08 -0400

    Add forms lecture

--- subjects/04 Forms/lecture.js
+++ subjects/04 Forms/lecture.js
@@ -3,12 +3,46 @@ import ReactDOM from "react-dom";
 import serializeForm from "form-serialize";
 
 class Forms extends React.Component {
+  state = { firstName: "Michael", lastName: "Jackson" };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const values = serializeForm(event.target, { hash: true });
+    console.log(values);
+  };
+
   render() {
     return (
       <div>
         <h1>Forms</h1>
-        <form>
-          <input type="text" />
+        <p>
+          <button
+            onClick={() => this.setState({ firstName: "Taylor" })}
+          >
+            Set the first name to "Taylor"
+          </button>
+        </p>
+        <p>
+          The name is "{this.state.firstName} {this.state.lastName}"
+        </p>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            name="firstName"
+            value={this.state.firstName}
+            onChange={event =>
+              this.setState({ firstName: event.target.value })
+            }
+          />
+          <input
+            type="text"
+            name="lastName"
+            defaultValue={this.state.lastName}
+            onChange={event =>
+              this.setState({ lastName: event.target.value })
+            }
+          />
+          <button>Submit</button>
         </form>
       </div>
     );
