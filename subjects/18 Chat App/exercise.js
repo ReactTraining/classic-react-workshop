0c4cb48ea5e42ea9efbab106aafd80cfe1a3ce04
Michael Jackson <mj@mjackson.me>
2018-04-06 16:34:46 -0700

    Add chat exercise

--- subjects/18 Chat App/exercise.js
+++ subjects/18 Chat App/exercise.js
@@ -42,55 +42,128 @@ unsubscribe() // stop listening for new messages
 The world is your oyster!
 */
 
-class Chat extends React.Component {
+import sortBy from "sort-by";
+
+class PinToBottom extends React.Component {
+  componentDidMount() {
+    this.autoScroll = true;
+    this.scrollToBottom();
+  }
+
+  componentDidUpdate() {
+    if (this.autoScroll) {
+      this.scrollToBottom();
+    }
+  }
+
+  scrollToBottom() {
+    const scroller = this.node;
+    scroller.scrollTop = scroller.scrollHeight;
+  }
+
+  handleScroll = event => {
+    // If they are scrolled back, do not auto scroll!
+    const node = event.target;
+    const { scrollTop, scrollHeight, clientHeight } = node;
+    const distanceToBottom = scrollHeight - (scrollTop + clientHeight);
+    this.autoScroll = distanceToBottom < 10;
+  };
+
   render() {
+    return (
+      <div
+        {...this.props}
+        ref={node => (this.node = node)}
+        onScroll={this.handleScroll}
+      />
+    );
+  }
+}
+
+class Chat extends React.Component {
+  state = {
+    auth: null,
+    messages: []
+  };
+
+  componentDidMount() {
+    login((error, auth) => {
+      this.setState({ auth });
+
+      subscribeToMessages(messages => {
+        this.setState({ messages });
+      });
+    });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    const { auth } = this.state;
+    const messageText = this.refs.message.value;
+
+    sendMessage(
+      auth.uid, // the auth.uid string
+      auth.github.username, // the username
+      auth.github.profileImageURL, // the user's profile image
+      messageText // the text of the message
+    );
+
+    event.target.reset();
+  };
+
+  render() {
+    const { auth, messages } = this.state;
+
+    if (auth == null) {
+      return <p>Authorizing...</p>;
+    }
+
+    const messageGroups = messages
+      .sort(sortBy("timestamp"))
+      .reduce((groups, message) => {
+        const lastGroup = groups[groups.length - 1];
+        const lastMessage =
+          lastGroup && lastGroup[lastGroup.length - 1];
+
+        if (lastMessage && lastMessage.uid === message.uid) {
+          // The same person is speaking!
+          lastGroup.push(message);
+        } else {
+          // A new person said something.
+          groups.push([message]);
+        }
+
+        return groups;
+      }, []);
+
     return (
       <div className="chat">
         <header className="chat-header">
           <h1 className="chat-title">HipReact</h1>
-          <p className="chat-message-count"># messages: 3</p>
+          <p className="chat-message-count">
+            # messages: {messages.length}
+          </p>
         </header>
-        <div className="messages">
+        <PinToBottom className="messages">
           <ol className="message-groups">
-            <li className="message-group">
-              <div className="message-group-avatar">
-                <img src="https://avatars1.githubusercontent.com/u/92839" />
-              </div>
-              <ol className="messages">
-                <li className="message">So, check it out:</li>
-                <li className="message">
-                  QA Engineer walks into a bar.
-                </li>
-                <li className="message">Orders a beer.</li>
-                <li className="message">Orders 0 beers.</li>
-                <li className="message">Orders 999999999 beers.</li>
-                <li className="message">Orders a lizard.</li>
-                <li className="message">Orders -1 beers.</li>
-                <li className="message">Orders a sfdeljknesv.</li>
-              </ol>
-            </li>
-            <li className="message-group">
-              <div className="message-group-avatar">
-                <img src="https://avatars2.githubusercontent.com/u/100200" />
-              </div>
-              <ol className="messages">
-                <li className="message">Haha</li>
-                <li className="message">
-                  Stop stealing other people's jokes :P
-                </li>
-              </ol>
-            </li>
-            <li className="message-group">
-              <div className="message-group-avatar">
-                <img src="https://avatars1.githubusercontent.com/u/92839" />
-              </div>
-              <ol className="messages">
-                <li className="message">:'(</li>
-              </ol>
-            </li>
+            {messageGroups.map((messages, index) => (
+              <li className="message-group" key={index}>
+                <div className="message-group-avatar">
+                  <img src={messages[0].avatarURL} />
+                </div>
+                <ol className="messages">
+                  {messages.map(message => (
+                    <li className="message" key={message._key}>
+                      {message.text}
+                    </li>
+                  ))}
+                </ol>
+              </li>
+            ))}
           </ol>
-        </div>
-        <form className="new-message-form">
+        </PinToBottom>
+        <form className="new-message-form" onSubmit={this.handleSubmit}>
           <div className="new-message">
             <input
               ref="message"
