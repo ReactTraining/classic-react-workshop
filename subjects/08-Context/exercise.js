cdc23394a1e017576c236604649f5abb720de540
Michael Jackson <mj@mjackson.me>
2018-04-12 15:34:37 -0500

    Finish context exercise

--- subjects/08-Context/exercise.js
+++ subjects/08-Context/exercise.js
@@ -19,38 +19,99 @@ import React from "react";
 import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
 
+// The branch is: chicago-adv-04-2018
+
 // SORRY, NOT SORRY!
 import { createContext } from "react-broadcast";
 
 const FormContext = createContext();
 
+// <FormContext.Provider value=...>...<FormContext.Provider>
+// <FormContext.Consumer>{value => ( ... )}</FormContext.Consumer>
+
 class Form extends React.Component {
+  values = {};
+
+  handleSubmit = () => {
+    if (this.props.onSubmit) {
+      this.props.onSubmit(this.values);
+    }
+  };
+
+  handleChange = (name, value) => {
+    this.values[name] = value;
+  };
+
   render() {
-    return <div>{this.props.children}</div>;
+    return (
+      <FormContext.Provider
+        value={{ submit: this.handleSubmit, change: this.handleChange }}
+      >
+        <div>{this.props.children}</div>
+      </FormContext.Provider>
+    );
   }
 }
 
 class SubmitButton extends React.Component {
   render() {
-    return <button>{this.props.children}</button>;
+    return (
+      <FormContext.Consumer>
+        {context => (
+          <button onClick={context.submit}>
+            {this.props.children}
+          </button>
+        )}
+      </FormContext.Consumer>
+    );
+  }
+}
+
+class CacheUpdater extends React.Component {
+  componentDidMount() {
+    this.props.context.change(this.props.name, this.props.defaultValue);
+  }
+
+  render() {
+    return null;
   }
 }
 
 class TextInput extends React.Component {
+  handleKeyDown = (event, submit) => {
+    if (event.key === "Enter") {
+      submit();
+    }
+  };
+
   render() {
     return (
-      <input
-        type="text"
-        name={this.props.name}
-        placeholder={this.props.placeholder}
-      />
+      <FormContext.Consumer>
+        {context => (
+          <span>
+            <CacheUpdater {...this.props} context={context} />
+            <input
+              type="text"
+              name={this.props.name}
+              placeholder={this.props.placeholder}
+              defaultValue={this.props.defaultValue}
+              onKeyDown={event =>
+                this.handleKeyDown(event, context.submit)
+              }
+              onChange={event =>
+                context.change(this.props.name, event.target.value)
+              }
+            />
+          </span>
+        )}
+      </FormContext.Consumer>
     );
   }
 }
 
 class App extends React.Component {
-  handleSubmit = () => {
-    alert("YOU WIN!");
+  handleSubmit = values => {
+    console.log(values);
   };
 
   render() {
@@ -62,8 +123,16 @@ class App extends React.Component {
 
         <Form onSubmit={this.handleSubmit}>
           <p>
-            <TextInput name="firstName" placeholder="First Name" />{" "}
-            <TextInput name="lastName" placeholder="Last Name" />
+            <TextInput
+              name="firstName"
+              placeholder="First Name"
+              defaultValue="Michael"
+            />{" "}
+            <TextInput
+              name="lastName"
+              placeholder="Last Name"
+              defaultValue="Jackson"
+            />
           </p>
           <p>
             <SubmitButton>Submit</SubmitButton>
