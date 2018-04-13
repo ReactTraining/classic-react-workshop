d45609f4099df61da37cad4d183b24d248fcd519
Michael Jackson <mj@mjackson.me>
2018-04-13 10:04:09 -0500

    Update HOC lecture

--- subjects/09-Higher-Order-Components/lecture.js
+++ subjects/09-Higher-Order-Components/lecture.js
@@ -3,29 +3,34 @@ import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
 import createMediaListener from "./utils/createMediaListener";
 
-const media = createMediaListener({
-  big: "(min-width : 1000px)",
-  tiny: "(max-width: 400px)"
-});
+const withMedia = (Component, queries) => {
+  const media = createMediaListener(queries);
+
+  return class extends React.Component {
+    state = {
+      media: media.getState()
+    };
+
+    componentDidMount() {
+      media.listen(media => this.setState({ media }));
+    }
+
+    componentWillUnmount() {
+      media.dispose();
+    }
+
+    render() {
+      return <Component {...this.props} media={this.state.media} />;
+    }
+  };
+};
 
 class App extends React.Component {
-  state = {
-    media: media.getState()
-  };
-
-  componentDidMount() {
-    media.listen(media => this.setState({ media }));
-  }
-
-  componentWillUnmount() {
-    media.dispose();
-  }
-
   render() {
-    const { media } = this.state;
+    const { media, bigMessage } = this.props;
 
     return media.big ? (
-      <h1>Hey, this is a big screen</h1>
+      <h1>{bigMessage}</h1>
     ) : media.tiny ? (
       <h6>tiny tiny tiny</h6>
     ) : (
@@ -34,7 +39,15 @@ class App extends React.Component {
   }
 }
 
-ReactDOM.render(<App />, document.getElementById("app"));
+const AppWithMedia = withMedia(App, {
+  big: "(min-width : 1000px)",
+  tiny: "(max-width: 400px)"
+});
+
+ReactDOM.render(
+  <AppWithMedia bigMessage="YELLO" />,
+  document.getElementById("app")
+);
 
 ////////////////////////////////////////////////////////////////////////////////
 // We can move all of that code into a higher-order component. A higher-order
