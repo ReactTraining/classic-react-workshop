a4ae21a81c677e8e0e3979f8e28898fb67b50923
Michael Jackson <mj@mjackson.me>
2018-04-28 09:05:30 -0700

    Update context solution + solution-extra

--- subjects/09-Context/solution-extra.js
+++ subjects/09-Context/solution-extra.js
@@ -19,135 +19,89 @@ import React from "react";
 import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
 
+const FormContext = React.createContext();
+
 class Form extends React.Component {
-  static childContextTypes = {
-    form: PropTypes.shape({
-      values: PropTypes.object.isRequired,
-      addField: PropTypes.func.isRequired,
-      change: PropTypes.func.isRequired,
-      submit: PropTypes.func.isRequired,
-      reset: PropTypes.func.isRequired
-    })
-  };
+  state = { values: {} };
 
-  defaultValues = {};
-
-  state = {
-    values: {}
-  };
-
-  getValues() {
-    return Object.assign({}, this.defaultValues, this.state.values);
-  }
-
-  getChildContext() {
-    return {
-      form: {
-        values: this.getValues(),
-        addField: (name, defaultValue) => {
-          this.defaultValues[name] = defaultValue;
-        },
-        change: (name, value) => {
-          this.setState(state => ({
-            values: {
-              ...state.values,
-              [name]: value
-            }
-          }));
-        },
-        submit: () => {
-          if (this.props.onSubmit)
-            this.props.onSubmit(this.getValues());
-        },
-        reset: () => {
-          this.setState({ values: {} });
-        }
+  handleChange = (name, value) => {
+    this.setState(state => ({
+      values: {
+        ...state.values,
+        [name]: value
       }
-    };
-  }
+    }));
+  };
 
-  render() {
-    return <div>{this.props.children}</div>;
-  }
-}
+  handleReset = () => {
+    this.setState({ values: {} });
+  };
 
-class SubmitButton extends React.Component {
-  static contextTypes = {
-    form: PropTypes.shape({
-      submit: PropTypes.func.isRequired
-    })
+  handleSubmit = () => {
+    if (this.props.onSubmit) this.props.onSubmit(this.state.values);
   };
 
   render() {
     return (
-      <button onClick={this.context.form.submit}>
-        {this.props.children}
-      </button>
+      <FormContext.Provider
+        value={{
+          change: this.handleChange,
+          reset: this.handleReset,
+          submit: this.handleSubmit,
+          values: this.state.values
+        }}
+      >
+        <div>{this.props.children}</div>
+      </FormContext.Provider>
     );
   }
 }
 
 class ResetButton extends React.Component {
-  static contextTypes = {
-    form: PropTypes.shape({
-      reset: PropTypes.func.isRequired
-    })
-  };
-
   render() {
     return (
-      <button onClick={this.context.form.reset}>
-        {this.props.children}
-      </button>
+      <FormContext.Consumer>
+        {context => (
+          <button onClick={context.reset}>{this.props.children}</button>
+        )}
+      </FormContext.Consumer>
+    );
+  }
+}
+
+class SubmitButton extends React.Component {
+  render() {
+    return (
+      <FormContext.Consumer>
+        {context => (
+          <button onClick={context.submit}>
+            {this.props.children}
+          </button>
+        )}
+      </FormContext.Consumer>
     );
   }
 }
 
 class TextInput extends React.Component {
-  static defaultProps = {
-    defaultValue: ""
-  };
-
-  static contextTypes = {
-    form: PropTypes.shape({
-      values: PropTypes.object.isRequired,
-      addField: PropTypes.func.isRequired,
-      change: PropTypes.func.isRequired,
-      submit: PropTypes.func.isRequired
-    })
-  };
-
-  componentWillMount() {
-    this.context.form.addField(
-      this.props.name,
-      this.props.defaultValue
-    );
-  }
-
-  handleChange = event => {
-    this.context.form.change(this.props.name, event.target.value);
-  };
-
-  handleKeyDown = event => {
-    if (event.key === "Enter") this.context.form.submit();
-  };
-
   render() {
-    const value = this.context.form.values.hasOwnProperty(
-      this.props.name
-    )
-      ? this.context.form.values[this.props.name]
-      : this.props.defaultValue;
-
     return (
-      <input
-        type="text"
-        name={this.props.name}
-        placeholder={this.props.placeholder}
-        onKeyDown={this.handleKeyDown}
-        onChange={this.handleChange}
-        value={value}
-      />
+      <FormContext.Consumer>
+        {context => (
+          <input
+            type="text"
+            name={this.props.name}
+            placeholder={this.props.placeholder}
+            value={context.values[this.props.name] || ""}
+            onChange={event => {
+              context.change(this.props.name, event.target.value);
+            }}
+            onKeyDown={event => {
+              if (event.key === "Enter") context.submit();
+            }}
+          />
+        )}
+      </FormContext.Consumer>
     );
   }
 }
@@ -166,20 +120,12 @@ class App extends React.Component {
 
         <Form onSubmit={this.handleSubmit}>
           <p>
-            <TextInput
-              name="firstName"
-              placeholder="First Name"
-              defaultValue="Michael"
-            />{" "}
-            <TextInput
-              name="lastName"
-              placeholder="Last Name"
-              defaultValue="Jackson"
-            />
+            <TextInput name="firstName" placeholder="First Name" />{" "}
+            <TextInput name="lastName" placeholder="Last Name" />
           </p>
           <p>
+            <ResetButton>Reset</ResetButton>{" "}
             <SubmitButton>Submit</SubmitButton>
-            <ResetButton>Reset</ResetButton>
           </p>
         </Form>
       </div>
--- subjects/09-Context/solution.js
+++ subjects/09-Context/solution.js
@@ -19,70 +19,72 @@ import React from "react";
 import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
 
+const FormContext = React.createContext();
+
 class Form extends React.Component {
-  static childContextTypes = {
-    form: PropTypes.shape({
-      submit: PropTypes.func.isRequired
-    }).isRequired
+  values = {};
+
+  handleChange = (name, value) => {
+    this.values[name] = value;
   };
 
-  getChildContext() {
-    return {
-      form: {
-        submit: () => {
-          if (this.props.onSubmit) this.props.onSubmit();
-        }
-      }
-    };
-  }
-
-  render() {
-    return <div>{this.props.children}</div>;
-  }
-}
-
-class SubmitButton extends React.Component {
-  static contextTypes = {
-    form: PropTypes.shape({
-      submit: PropTypes.func.isRequired
-    }).isRequired
+  handleSubmit = () => {
+    if (this.props.onSubmit) this.props.onSubmit(this.values);
   };
 
   render() {
     return (
-      <button onClick={this.context.form.submit}>
-        {this.props.children}
-      </button>
+      <FormContext.Provider
+        value={{
+          change: this.handleChange,
+          submit: this.handleSubmit
+        }}
+      >
+        <div>{this.props.children}</div>
+      </FormContext.Provider>
+    );
+  }
+}
+
+class SubmitButton extends React.Component {
+  render() {
+    return (
+      <FormContext.Consumer>
+        {context => (
+          <button onClick={context.submit}>
+            {this.props.children}
+          </button>
+        )}
+      </FormContext.Consumer>
     );
   }
 }
 
 class TextInput extends React.Component {
-  static contextTypes = {
-    form: PropTypes.shape({
-      submit: PropTypes.func.isRequired
-    }).isRequired
-  };
-
-  handleKeyDown = event => {
-    if (event.key === "Enter") this.context.form.submit();
-  };
-
   render() {
     return (
-      <input
-        type="text"
-        name={this.props.name}
-        placeholder={this.props.placeholder}
-        onKeyDown={this.handleKeyDown}
-      />
+      <FormContext.Consumer>
+        {context => (
+          <input
+            type="text"
+            name={this.props.name}
+            placeholder={this.props.placeholder}
+            onChange={event => {
+              context.change(this.props.name, event.target.value);
+            }}
+            onKeyDown={event => {
+              if (event.key === "Enter") context.submit();
+            }}
+          />
+        )}
+      </FormContext.Consumer>
     );
   }
 }
 
 class App extends React.Component {
-  handleSubmit = () => {
-    alert("YOU WIN!");
+  handleSubmit = values => {
+    console.log(values);
   };
 
   render() {
