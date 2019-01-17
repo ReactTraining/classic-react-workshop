1b78e9b90a8b5457d6e11c341789d8c118732dd0
Michael Jackson <michael@jackson.us>
2019-01-17 15:28:42 -0800

    Render opt lecture

--- subjects/09-Render-Optimizations/lecture.js
+++ subjects/09-Render-Optimizations/lecture.js
@@ -2,8 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
 
-class TodoItem extends React.Component {
+class TodoItem extends React.PureComponent {
   state = { done: false };
+
+  //   shouldComponentUpdate(nextProps, nextState) {
+  //     return (
+  //       nextProps.body !== this.props.body ||
+  //       nextState.done !== this.state.done
+  //     );
+  //   }
 
   render() {
     return (
@@ -78,22 +85,6 @@ class TodoList extends React.Component {
 }
 
 ReactDOM.render(
-  <TodoList initialLength={100} />,
+  <TodoList initialLength={10000} />,
   document.getElementById("app")
 );
-
-///////////////////////////////////////////////////////////////////////////////
-// Rendering large lists can be super slow. This is an old UI problem.
-
-///////////////////////////////////////////////////////////////////////////////
-// One possible solution is to only render the stuff that's actually in the
-// view. Native mobile frameworks have been doing this for years:
-//
-// https://developer.apple.com/library/ios/documentation/UIKit/Reference/UITableView_Class/index.html
-
-///////////////////////////////////////////////////////////////////////////////
-// I'd really like to do this in my web app! What does it look like when we
-// try to do this with imperative JavaScript?
-//
-// https://github.com/airbnb/infinity
-// https://github.com/emberjs/list-view
