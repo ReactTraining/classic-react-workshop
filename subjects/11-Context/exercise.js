a42986f409cd5c043bdfc68c9aded0df71a654ba
Michael Jackson <michael@jackson.us>
2019-01-24 13:37:17 -0800

    Select + context solution

--- subjects/11-Context/exercise.js
+++ subjects/11-Context/exercise.js
@@ -19,33 +19,128 @@ import React from "react";
 import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
 
+const FormContext = React.createContext();
+
 class Form extends React.Component {
+  values = {};
+
+  handleChange = (key, value) => {
+    this.values[key] = value;
+  };
+
+  handleSubmit = () => {
+    if (this.props.onSubmit) {
+      this.props.onSubmit(this.values);
+    }
+  };
+
+  resetHandlers = [];
+
+  addResetHandler = handler => {
+    this.resetHandlers.push(handler);
+
+    return () => {
+      this.resetHandlers = this.resetHandlers.filter(
+        h => h !== handler
+      );
+    };
+  };
+
+  handleReset = () => {
+    this.resetHandlers.forEach(handler => handler());
+  };
+
   render() {
-    return <div>{this.props.children}</div>;
+    return (
+      <FormContext.Provider
+        value={{
+          change: this.handleChange,
+          submit: this.handleSubmit,
+          onReset: this.addResetHandler,
+          reset: this.handleReset
+        }}
+      >
+        <div>{this.props.children}</div>
+      </FormContext.Provider>
+    );
+  }
+}
+
+class ResetButton extends React.Component {
+  static contextType = FormContext;
+
+  render() {
+    return (
+      <button onClick={this.context.reset}>
+        {this.props.children}
+      </button>
+    );
   }
 }
 
 class SubmitButton extends React.Component {
+  static contextType = FormContext;
+
   render() {
-    return <button>{this.props.children}</button>;
+    return (
+      <button onClick={this.context.submit}>
+        {this.props.children}
+      </button>
+    );
   }
 }
 
 class TextInput extends React.Component {
+  static contextType = FormContext;
+
+  state = { value: this.props.defaultValue };
+
+  changeValue = value => {
+    this.setState({ value });
+    this.context.change(this.props.name, value);
+  };
+
+  handleChange = event => {
+    this.changeValue(event.target.value);
+  };
+
+  handleKeyDown = event => {
+    if (event.key === "Enter") {
+      const form = this.context;
+      form.submit();
+    }
+  };
+
+  handleReset = () => {
+    this.changeValue(this.props.defaultValue);
+  };
+
+  componentDidMount() {
+    this.context.change(this.props.name, this.state.value);
+    this.removeResetHandler = this.context.onReset(this.handleReset);
+  }
+
+  componentWillUnmount() {
+    this.removeResetHandler();
+  }
+
   render() {
     return (
       <input
         type="text"
         name={this.props.name}
+        value={this.state.value}
         placeholder={this.props.placeholder}
+        onKeyDown={this.handleKeyDown}
+        onChange={this.handleChange}
       />
     );
   }
 }
 
 class App extends React.Component {
-  handleSubmit = () => {
-    alert("YOU WIN!");
+  handleSubmit = values => {
+    console.log(values);
   };
 
   render() {
@@ -57,11 +152,20 @@ class App extends React.Component {
 
         <Form onSubmit={this.handleSubmit}>
           <p>
-            <TextInput name="firstName" placeholder="First Name" />{" "}
-            <TextInput name="lastName" placeholder="Last Name" />
+            <TextInput
+              name="firstName"
+              defaultValue="Michael"
+              placeholder="First Name"
+            />{" "}
+            <TextInput
+              name="lastName"
+              defaultValue="Jackson"
+              placeholder="Last Name"
+            />
           </p>
           <p>
-            <SubmitButton>Submit</SubmitButton>
+            <SubmitButton>Submit</SubmitButton>{" "}
+            <ResetButton>Reset</ResetButton>
           </p>
         </Form>
       </div>
--- subjects/20-Select/exercise.js
+++ subjects/20-Select/exercise.js
@@ -13,16 +13,70 @@ class Select extends React.Component {
   static propTypes = {
     onChange: PropTypes.func,
     value: PropTypes.any,
-    defaultValue: PropTypes.any
+    defaultValue: PropTypes.any,
+    readOnly: PropTypes.bool
   };
 
+  static defaultProps = {
+    readOnly: false
+  };
+
+  state = {
+    showOptions: false,
+    value: this.props.defaultValue
+  };
+
+  toggleOptions = () =>
+    this.setState({ showOptions: !this.state.showOptions });
+
+  componentDidMount() {
+    if (
+      this.isControlled() &&
+      !this.props.onChange &&
+      !this.props.readOnly
+    ) {
+      console.warn(
+        "This <Select> has a `value` but no `onChange` prop, so it'll be read-only"
+      );
+    }
+  }
+
+  selectValue = value => {
+    if (this.isControlled()) {
+      this.props.onChange(value);
+    } else {
+      this.setState({ value });
+    }
+  };
+
+  isControlled = () => this.props.value != null;
+
   render() {
+    const value = this.isControlled()
+      ? this.props.value
+      : this.state.value;
+
+    let label;
+    React.Children.forEach(this.props.children, child => {
+      if (child.props.value === value) {
+        label = child.props.children; // the element's text!
+      }
+    });
+
     return (
-      <div className="select">
+      <div className="select" onClick={this.toggleOptions}>
         <div className="label">
-          label <span className="arrow">▾</span>
+          {label} <span className="arrow">▾</span>
         </div>
-        <div className="options">{this.props.children}</div>
+        {this.state.showOptions && (
+          <div className="options">
+            {React.Children.map(this.props.children, child =>
+              React.cloneElement(child, {
+                _onSelect: () => this.selectValue(child.props.value)
+              })
+            )}
+          </div>
+        )}
       </div>
     );
   }
@@ -30,17 +84,27 @@ class Select extends React.Component {
 
 class Option extends React.Component {
   render() {
-    return <div className="option">{this.props.children}</div>;
+    return (
+      <div className="option" onClick={this.props._onSelect}>
+        {this.props.children}
+      </div>
+    );
   }
 }
 
 class App extends React.Component {
-  state = {
-    selectValue: "dosa"
-  };
+  state = { selectValue: "dosa" };
 
   setToMintChutney = () => {
     this.setState({ selectValue: "mint-chutney" });
+  };
+
+  handleChange = value => {
+    if (value === "hamburger") {
+      alert("You are at the wrong restaurant");
+    } else {
+      this.setState({ selectValue: value });
+    }
   };
 
   render() {
@@ -55,6 +119,7 @@ class App extends React.Component {
           <Option value="tandoori-chicken">Tandoori Chicken</Option>
           <Option value="dosa">Dosa</Option>
           <Option value="mint-chutney">Mint Chutney</Option>
+          <Option value="hamburger">Hamburger</Option>
         </Select>
 
         <h2>Controlled</h2>
@@ -68,12 +133,13 @@ class App extends React.Component {
 
         <Select
           value={this.state.selectValue}
-          onChange={value => this.setState({ selectValue: value })}
+          onChange={this.handleChange}
         >
           <Option value="tikka-masala">Tikka Masala</Option>
           <Option value="tandoori-chicken">Tandoori Chicken</Option>
           <Option value="dosa">Dosa</Option>
           <Option value="mint-chutney">Mint Chutney</Option>
+          <Option value="hamburger">Hamburger</Option>
         </Select>
       </div>
     );
