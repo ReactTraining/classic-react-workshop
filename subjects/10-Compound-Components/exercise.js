3df1bd7dc1ca397f0278ac5adb056fea15d519b4
Brad Westfall <brad@azpixels.com>
2019-02-28 17:00:06 -0700

    hooks for 10 (compound comps)

--- subjects/10-Compound-Components/exercise.js
+++ subjects/10-Compound-Components/exercise.js
@@ -19,70 +19,49 @@
 ////////////////////////////////////////////////////////////////////////////////
 import React from "react";
 import ReactDOM from "react-dom";
-import PropTypes from "prop-types";
 
-class RadioGroup extends React.Component {
-  static propTypes = {
-    defaultValue: PropTypes.string
-  };
-
-  render() {
-    return <div>{this.props.children}</div>;
-  }
+function RadioGroup({ children }) {
+  return <div>{children}</div>;
 }
 
-class RadioOption extends React.Component {
-  static propTypes = {
-    value: PropTypes.string
-  };
-
-  render() {
-    return (
-      <div>
-        <RadioIcon isSelected={false} /> {this.props.children}
-      </div>
-    );
-  }
+function RadioOption({ children }) {
+  return (
+    <div>
+      <RadioIcon isSelected={false} /> {children}
+    </div>
+  );
 }
 
-class RadioIcon extends React.Component {
-  static propTypes = {
-    isSelected: PropTypes.bool.isRequired
-  };
-
-  render() {
-    return (
-      <div
-        style={{
-          borderColor: "#ccc",
-          borderWidth: 3,
-          borderStyle: this.props.isSelected ? "inset" : "outset",
-          height: 16,
-          width: 16,
-          display: "inline-block",
-          cursor: "pointer",
-          background: this.props.isSelected ? "rgba(0, 0, 0, 0.05)" : ""
-        }}
-      />
-    );
-  }
+function RadioIcon({ isSelected }) {
+  return (
+    <div
+      style={{
+        borderColor: "#ccc",
+        borderWidth: 3,
+        borderStyle: isSelected ? "inset" : "outset",
+        height: 16,
+        width: 16,
+        display: "inline-block",
+        cursor: "pointer",
+        background: isSelected ? "rgba(0, 0, 0, 0.05)" : ""
+      }}
+    />
+  );
 }
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>♬ It's about time that we all turned off the radio ♫</h1>
+function App() {
+  return (
+    <div>
+      <h1>♬ It's about time that we all turned off the radio ♫</h1>
 
-        <RadioGroup defaultValue="fm">
-          <RadioOption value="am">AM</RadioOption>
-          <RadioOption value="fm">FM</RadioOption>
-          <RadioOption value="tape">Tape</RadioOption>
-          <RadioOption value="aux">Aux</RadioOption>
-        </RadioGroup>
-      </div>
-    );
-  }
+      <RadioGroup defaultValue="fm">
+        <RadioOption value="am">AM</RadioOption>
+        <RadioOption value="fm">FM</RadioOption>
+        <RadioOption value="tape">Tape</RadioOption>
+        <RadioOption value="aux">Aux</RadioOption>
+      </RadioGroup>
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.getElementById("app"));
